383d572516d85bd6ed6b6ad1a50f573666117cbc
abc20210708 <gotqlc2670@naver.com>
2023-02-02 13:53:50 +0900

    Update Q3-2.py

--- greedy/chap/Q3-2.py
+++ greedy/chap/Q3-2.py
@@ -12,6 +12,11 @@ n, m, k= map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
 data = list(map(int, input().split()))
 
+
+## n, m, k = 5, 7, 2
+
+## data = [3, 4, 3, 4, 3]
+
 data.sort() # 입력받은 수들 정리하기
 first = data[n- 1] # 가장 큰 수
 second = data[n - 2] # 두 번째로 큰 수
