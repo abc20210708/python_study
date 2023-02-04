3d9bc93dddd16d07460708c6c4042c609730707c
abc20210708 <gotqlc2670@naver.com>
2023-02-04 15:38:21 +0900

    Update Q07.py

--- implementation/ex/Q07.py
+++ implementation/ex/Q07.py
@@ -6,9 +6,27 @@
 
 '''
 
-n = "7755"
-# 길이는 6
+n = "123402"
 
+length = len(n)
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length // 2) :
+    summary += int(n[i])
+
+for i in range(length // 2, length) :
+    summary -= int(n[i])
+    
+    
+# 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검색
+if summary == 0:
+    print("LUCKY")
+else:
+    print("READY")    
+
+
+'''
 num1, num2 = 0 , 0
 for i in range(len(n)) :
     if i >= len(n) / 2 :
@@ -20,3 +38,4 @@ if num1 == num2 :
     print("LUCKY")
 else :
     print("READY")
+'''
