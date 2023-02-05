0b87b14454c90d94a7c7c84b5fe4792f990a7547
abc20210708 <gotqlc2670@naver.com>
2023-02-05 21:17:24 +0900

    Update Q5-10.py

--- dfs_bfs/chap/Q5-10.py
+++ dfs_bfs/chap/Q5-10.py
@@ -1,12 +1,19 @@
 # 음료수 얼려 먹기
 
 # N, M을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
+#n, m = map(int, input().split())
+n, m = 3, 3
 
 # 2차원 리스트의 맵 정보 입력받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+#graph = []
+#for i in range(n):
+#    graph.append(list(map(int, input())))
+    
+    
+graph = [[0, 0, 1],
+         [0, 1, 0],
+         [1, 0, 1]
+         ]
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs(x, y):
