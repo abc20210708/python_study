f10f0b0d200e30471c6878f63deeda978977ce21
abc20210708 <gotqlc2670@naver.com>
2023-02-06 15:21:00 +0900

    18. 괄호 변환

--- dfs_bfs/ex/Q15.py
+++ dfs_bfs/ex/Q15.py
@@ -1,18 +1,24 @@
 # 특정 거리의 도시 찾기
 
+# 참고 블로그 https://soopeach.tistory.com/14
+
 from collections import deque
 
 # 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번화
 n, m, k, x = map(int, input().split())
+
+# 도시 1부터 확인하기 위해 그래프 0번째 칸은 빈칸으로 생성
 graph = [[] for _ in range(n + 1)]
 
 # 모든 도로 정보 입력받기
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
+# 예제 입력 1 기준 아래와 같이 그래프 생성
+# [[], [2, 3],[3, 4],[],[]]
     
 # 모든 도시에 대한 최단 거리 초기화
-distance = [-1] * (n + 1)
+distance = [-1] * (n + 1) 
 distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
 
 # 너비 우선 탐색(BFS) 수행
---
+++ dfs_bfs/ex/Q18.py
@@ -0,0 +1,52 @@
+# 괄호 변환
+
+# "균현잡힌 괄호 문자열"의 인덱스 변환
+def balanced_index(p):
+    cnt = 0 # 왼쪽 괄호의 개수
+    for i in range(len(p)):
+        if p[i] == '(':
+            cnt += 1
+        else :
+            cnt -= 1
+        if cnt == 0:
+            return i
+        
+# 올바른 괄호 문자열인지 판단
+def check_proper(p):
+    cnt = 0 # 왼쪽 괄호의 개수
+    for i in p :
+        if i == '(':
+            cnt += 1
+        else: # cnt가 0이 된 상태에서 )가 먼저 나오면 쌍이 맞지 않음!
+            if cnt == 0: # 쌍이 맞지 않은 경우 False 반환
+                return False
+            cnt -= 1
+    return True # 쌍이 맞는 경우 True 반환
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1:]
+    # "올바른 괄호 문자열"이면, v에 대해 함수를 수행한 결과를 붙여 반환
+    if check_proper(u):
+        answer = u + solution(v)
+    # 올바른 괄호 문자열이 아니라면 아래의 과정을 수행
+    else :
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1]) # 첫 번째와 마지막 문자를 제거
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else :
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+                
+    
+
+print(solution(")("))
