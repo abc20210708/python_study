229194b571ffd78023347fd8767a1a3a40aac035
abc20210708 <gotqlc2670@naver.com>
2023-02-06 16:28:12 +0900

    Update Q6-10.py

--- sorting/chap/Q6-10.py
+++ sorting/chap/Q6-10.py
@@ -1,10 +1,11 @@
 # 위에서 아래로
 
-N = int(input())
+#N = int(input())
+N = 3
 
-array = []
-for i in range(N):
-    array.append(int(input()))
+array = [15, 27, 12]
+#for i in range(N):
+#    array.append(int(input()))
     
 # 파이썬 기본 라이브러리를 이용해 정렬 수행
 array = sorted(array, reverse=True)
