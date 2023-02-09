31058f4b30a99197bcfbed6acba290116d473f5a
abc20210708 <gotqlc2670@naver.com>
2023-02-09 15:51:50 +0900

    모험가 길드 다시 풀기

--- hello.py
+++ hello.py
@@ -1,27 +1,22 @@
 
-# 1이 될 때까지 효율적으로 다시 풀기
+# 모험가 길드 다시 풀기
 
 '''
 
 '''
-n, k = 25, 3
+n = 5
+data = [2, 3, 1, 2, 2]
+
+data.sort()
+
+cnt = 0
 result = 0
 
-while True:
-    # (N == K 로 나우어 떨어지는 수)가 될 떄까지 1씩 빼기
-    target = (n // k) * k
-    result += (n - target)
-    n = target
-    
-    # N이 K보다 작을 때(더 이상 나울 수 없을 때) 반복문 탈출
-    if n < k:
-        break
-    # K로 나누기
-    result += 1
-    n //= k
-    
-# 마지막으로 남은 수에 대하여 1씩 빼기
-result += (n - 1)
+for i in data:
+    cnt += 1
+    if cnt >= i:
+        result += 1
+        cnt = 0
     
 print(result)
         
