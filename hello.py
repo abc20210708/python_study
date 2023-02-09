32d9d16345f4cceb005556929c8327a5de17f9b6
abc20210708 <gotqlc2670@naver.com>
2023-02-09 16:31:15 +0900

    문자열 뒤집기 다시 풀기

--- hello.py
+++ hello.py
@@ -1,21 +1,39 @@
 
-# 곱하기 혹은 더하기 다시 풀기
+# 문자열 뒤집기 다시 풀기
 
-'''
+#import sys
 
-'''
-data = "567"
+# 문자열 입력
+#s = list(sys.stdin.readline().rstrip())
+s = "0001100"
 
-result =  int(data[0])
+# 0이 연속된 부분
+cnt0 = 0
+# 1이 연속된 부분
+cnt1 = 0
 
-for i in range(1, len(data)):
-    num = int(data[i])
-    if result <= 1 or num <= 1:
-        result += num
-    else:
-        result *= num
+# 첫 번째 정수는 곧바로 연속된 부분으로 추가
+if (s[0] == '0') :
+    cnt0 += 1
+else:
+    cnt1 += 1
     
-print(result)
+# 정수를 순서대로 탐색하며, 연속된 정수가 다른 정수로 바뀔 때,
+# 연속된 부분의 개수 추가
+for i in range(len(s) - 1):
+    # 연속된 정수가 다음 수에서 다른 정수로 바뀔 때
+    if(s[i] != s[i + 1]):
+        # 다음 수에서 0으로 바뀌는 경우
+        if(s[i + 1] == '0'):
+            cnt0 += 1
+        # 다음 수에서 1로 바뀌는 경우
+        else :
+            cnt1 += 1
+            
+# 연속된 부분의 개수가 적은 값을 출력
+print(min(cnt0, cnt1))
+
+
         
     
 
