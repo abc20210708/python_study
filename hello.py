6cae1add8d116edb3d496fff6de86d3cca243934
abc20210708 <gotqlc2670@naver.com>
2023-02-10 13:43:19 +0900

    럭키 스트레이트 다시 풀기

--- hello.py
+++ hello.py
@@ -1,16 +1,22 @@
 
-# 시각 다시 풀기
+# 럭키 스트레이트 다시 풀기
 
-hour = 5
-cnt = 0
+n = 123402
 
-for i in range(hour + 1):
-    for j in range(60):
-        for k in range(60):
-            #매 시각 안에 '3'이 포함되어 있다면
-            # 카운트 증가
-            if '3' in str(i) + str(j) + str(k):
-                cnt += 1
+target = str(n)
+result = 0
+length = (len(target)-1) // 2 + 1
+
+for i in range(length):
+    result += int(target[i])
+
+for i in range(length, len(target)):
+    result -= int(target[i])  
+    
+if result == 0 :
+    print("LUCKY")
+else :
+    print("READY")
+    
                 
-print(cnt)
 
