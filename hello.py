f20854cde2842c94ffac93578e6cdf5b25e7657a
abc20210708 <gotqlc2670@naver.com>
2023-02-11 16:12:00 +0900

    특정 거리의 도시 찾기 다시 풀기

--- hello.py
+++ hello.py
@@ -1,43 +1,35 @@
 
-# 미로 탈출 다시 풀기
+# 특정 거리의 도시 찾기 다시 풀기
 from collections import deque
 
-n, m = 3, 3
+n, m, k, x = 4, 4, 2, 1
 
 graph = [
-    [1,1,0],
-    [0,1,0],
-    [0,1,1]
-]
+         [],
+         [2, 3],
+         [3, 4],
+         [],
+         []
+         ]
 
+# 최단 거리 지정
+distance = [-1] * (n + 1)
+# 출발 지정
+distance[x] = 0
 
-# 이동할 네 방향 정의
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    
-    while queue:
-        x, y = queue.popleft()
-        
-        # 현재 위치에서 네 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+q = deque([x])
+while q:
+    now = q.popleft()
+    for i in graph[now]:
+        if distance[i] == -1:
+            distance[i] = distance[now] + 1
+            q.append(i)
             
-            if nx <0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return graph[n - 1][m - 1]
-
-
-
-print(bfs(0, 0))
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+        
+if check == False:
+    print(-1)
