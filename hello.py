2a9baf2af31aea0ce0734f76516f9bc3ebf8225c
abc20210708 <gotqlc2670@naver.com>
2023-02-11 13:40:51 +0900

    DFS 메서드 정의 다시 풀기

--- hello.py
+++ hello.py
@@ -1,24 +1,32 @@
 
-# 팩토리얼 예제 다시 풀기
+# DFS 메서드 정의 다시 풀기
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-# 반복적으로 구현한 팩토리얼
-def fac(n):
-    result = 1
-    # 1부터 n까지 수를 차례대로 곱하기
-    for i in range(1, n + 1):
-        result *= i
-    return result
 
-print(fac(5))
-    
-    
-# 재귀로 구현
-def fac1(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * fac1(n - 1)
+# 각 노드와 연결된 정보를 리스트 자료형으로 표현
+# (2차원 리스트)    
+graph = [
+    [],
+    [2, 3, 8], # 1
+    [1, 7],    # 2
+    [1, 4, 5], # 3
+    [3, 5],    # 4
+    [3, 4],    # 5
+    [7],       # 6
+    [2, 6, 8], # 7
+    [1, 7]     # 8  
+]       
 
-print(fac1(5))
-                
+# 각 노드가 바운된 정보를 리스트 자료형으로 표현
+# (1차원 리스트)
+visited = [False] * 9
 
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
