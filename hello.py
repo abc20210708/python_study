1315c93457b362a59ca0c184f060298c52b7a973
abc20210708 <gotqlc2670@naver.com>
2023-02-11 13:13:49 +0900

    팩토리얼 예제 다시 풀기

--- hello.py
+++ hello.py
@@ -1,22 +1,24 @@
 
-# 문자열 재정렬 다시 풀기
+# 팩토리얼 예제 다시 풀기
 
-n = 123402
+# 반복적으로 구현한 팩토리얼
+def fac(n):
+    result = 1
+    # 1부터 n까지 수를 차례대로 곱하기
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
-target = str(n)
-result = 0
-length = (len(target)) // 2
-
-for i in range(length):
-    result += int(target[i])
-
-for i in range(length, len(target)):
-    result -= int(target[i])  
+print(fac(5))
     
-if result == 0 :
-    print("LUCKY")
-else :
-    print("READY")
     
+# 재귀로 구현
+def fac1(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * fac1(n - 1)
+
+print(fac1(5))
                 
 
