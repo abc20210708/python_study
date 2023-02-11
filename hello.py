9e3e86cdf5a5a090c396a64ea5baf12676d5e7fa
abc20210708 <gotqlc2670@naver.com>
2023-02-11 17:09:08 +0900

    괄호 변환 다시 풀기

--- hello.py
+++ hello.py
@@ -1,35 +1,58 @@
 
-# 특정 거리의 도시 찾기 다시 풀기
-from collections import deque
+# 괄호 변환 다시 풀기
 
-n, m, k, x = 4, 4, 2, 1
 
-graph = [
-         [],
-         [2, 3],
-         [3, 4],
-         [],
-         []
-         ]
+# 균형잡힌 괄호 문자열의 인덱스 확인
+def balance_idx(p):
+    cnt = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            cnt += 1
+        else :
+            cnt -= 1
+        if cnt == 0:
+            return i
 
-# 최단 거리 지정
-distance = [-1] * (n + 1)
-# 출발 지정
-distance[x] = 0
 
-q = deque([x])
-while q:
-    now = q.popleft()
-    for i in graph[now]:
-        if distance[i] == -1:
-            distance[i] = distance[now] + 1
-            q.append(i)
-            
-check = False
-for i in range(1, n + 1):
-    if distance[i] == k:
-        print(i)
-        check = True
-        
-if check == False:
-    print(-1)
+# 올바른 괄호 문자열인지 판단
+def check_proper(p):
+    cnt = 0
+    for i in p:
+        if i == '(':
+            cnt += 1
+        else :
+            if cnt == 0:
+                return False
+            cnt -= 1
+    return True
+
+
+
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    
+    idx = balance_idx(p)
+    u = p[:idx + 1]
+    v = p[idx + 1:]
+    
+# "올바른 괄호 문자열"이면
+# v에 대해 함수를 수행한 결과를 붙여 반환
+    if check_proper(u):
+        answer = u + solution(v)
+    else :
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+
+print(solution("()))((()"))
