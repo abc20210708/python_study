c6d1ad7df0a719d4b83a149eaf60fb7fb710ff24
abc20210708 <gotqlc2670@naver.com>
2023-02-11 15:31:50 +0900

    미로 탈출 다시 풀기

--- hello.py
+++ hello.py
@@ -1,41 +1,43 @@
 
-# 음료수 얼려 먹기 다시 풀기
-
+# 미로 탈출 다시 풀기
+from collections import deque
 
 n, m = 3, 3
 
-# 2차원 리스트의 맵 정보 
 graph = [
-    [0,0,1],
+    [1,1,0],
     [0,1,0],
-    [1,0,1]
+    [0,1,1]
 ]
 
+
+# 이동할 네 방향 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
     
-
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] == 0:
-        # 해당 노드 방문 처리
-        graph[x][y] = 1
-        # 상,하,좌,우의 위치도 모두 재귀적으로 호출
-        dfs(x -1, y)
-        dfs(x + 1, y) 
-        dfs(x, y - 1)
-        dfs(x, y + 1)
-        return True
-    return False
-
-# 모든 위치에 음료 채우기
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
+    while queue:
+        x, y = queue.popleft()
         
-print(result)
-        
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx <0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+
+
+print(bfs(0, 0))
