e8b728e12fc731f1d56bf11a347a3f53972e9ab7
abc20210708 <gotqlc2670@naver.com>
2023-02-12 16:41:57 +0900

    파이썬 장점을 살린 퀵 정렬 다시 풀기

--- hello.py
+++ hello.py
@@ -1,33 +1,19 @@
 
-# 퀵 정렬 다시 풀기
+# 파이썬 장점을 살린 퀵 정렬 다시 풀기
 
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
-def quick_sort(array, start, end):
-    if start >= end: # 원소가 1개인 경우 종료
-        return
-    pivot = start # 피벗은 첫 번째 원소
-    left = start + 1
-    right = end
+def quick_sort(array):
+    # 리스트 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
     
-    while left <= right:
-        # 피벗보다 큰 데이터를 찾을 때까지 반복
-        while left <= end and array[left] <= array[pivot]:
-            left += 1
-        # 피벗보다 작은 데이터를 찾을 때까지 반복
-        while right > start and array[right] >= array[pivot]:
-            right -= 1
-        if left > right :
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[left], array[right] = array[right], array[left]
+    pivot = array[0] # 피벗은 첫 번째 원소
+    tail = array[1: ]# 피벗을 제외한 리스트
     
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
-    
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
-quick_sort(array, 0, len(array) -1)
-print(array)
-
-
+print(quick_sort(array))
 
