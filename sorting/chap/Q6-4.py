f4abb66b65cbbf2b0e66090cae5372ec4b04665f
abc20210708 <gotqlc2670@naver.com>
2023-02-12 16:11:09 +0900

    Update Q6-4.py

--- sorting/chap/Q6-4.py
+++ sorting/chap/Q6-4.py
@@ -1,5 +1,14 @@
 # 퀵 정렬
 
+# 참고 블로그 https://gyoogle.dev/blog/algorithm/Quick%20Sort.html
+
+'''
+하나의 리스트를 피벗을 기준으로 두 개의 비균등한 크기로 분할하고,
+분할된 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를 
+합하여 전체가 정렬된 리스트가 되게 하는 방법
+
+'''
+
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quick_sort(array, start, end):
@@ -20,7 +29,7 @@ def quick_sort(array, start, end):
             array[right], array[pivot] = array[pivot], array[right]
         else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
             array[left], array[right] = array[right], array[left]
-    #ㅂ 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
     quick_sort(array, start, right -1)
     quick_sort(array, right +1, end)
     
