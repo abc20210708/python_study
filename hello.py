29dfebd27ee306ae482b709524927963aebda852
abc20210708 <gotqlc2670@naver.com>
2023-02-12 15:45:38 +0900

    삽입 정렬 다시 풀기

--- hello.py
+++ hello.py
@@ -1,16 +1,32 @@
 
-# 선택 정렬 다시 풀기
+# 삽입 정렬 다시 풀기
+
 
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(array)):
-    min_idx = i # 가장 작은 원소의 인덱스
-    for j in range(i + 1, len(array)):
-        if array[min_idx] > array[j]:
-            min_idx = j
-    array[i], array[min_idx] = array[min_idx], array[i]
-    print(array)
-    
-print("최종 선택 정렬: ")    
-print(array)    
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): #인덱스i부터 1까지 -1씩 감소
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:
+            break
+        
+print("최종 삽입 정렬 : ")
+print(array)
+
+
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        j = i
+        while j > 0 and arr[j - 1]  > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1] 
+            j -= 1
+    return arr
+
+
+print(insertion_sort(arr))
+
+
 
