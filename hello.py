7651c1db885f53781a18545fe7cb9abfbd5c72e4
abc20210708 <gotqlc2670@naver.com>
2023-02-12 16:16:45 +0900

    퀵 정렬 다시 풀기

--- hello.py
+++ hello.py
@@ -1,32 +1,33 @@
 
-# 삽입 정렬 다시 풀기
+# 퀵 정렬 다시 풀기
 
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
-
-for i in range(1, len(array)):
-    for j in range(i, 0, -1): #인덱스i부터 1까지 -1씩 감소
-        if array[j] < array[j - 1]:
-            array[j], array[j - 1] = array[j - 1], array[j]
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = start # 피벗은 첫 번째 원소
+    left = start + 1
+    right = end
+    
+    while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right :
+            array[right], array[pivot] = array[pivot], array[right]
         else:
-            break
-        
-print("최종 삽입 정렬 : ")
+            array[left], array[right] = array[right], array[left]
+    
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+    
+
+quick_sort(array, 0, len(array) -1)
 print(array)
-
-
-arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
-def insertion_sort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        j = i
-        while j > 0 and arr[j - 1]  > arr[j]:
-            arr[j - 1], arr[j] = arr[j], arr[j - 1] 
-            j -= 1
-    return arr
-
-
-print(insertion_sort(arr))
 
 
 
