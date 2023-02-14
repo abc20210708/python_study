252d6a0a77c14e17f1c178ae218fef68e53bbd97
abc20210708 <gotqlc2670@naver.com>
2023-02-14 14:25:27 +0900

    큰 수의 법칙 다시 풀기

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 # 큰 수의 법칙 다시 풀기
 
 # 배열의 크기 N, 숫자 더해지는 횟수 M, 그리고 연속할 수 있는 수 K
-n, m, k = 5, 8, 3
-arr = [2, 4, 5, 4, 6]
+n, m, k = 5, 7, 2
+arr = [3, 4, 3, 4, 3]
 
 arr.sort()
 
@@ -12,15 +12,11 @@ second = arr[-2]
 
 result = 0
 
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+cnt1 = (m // (k + 1)) * k
+cnt1 += (m % (k + 1))
+cnt2 = (m - cnt1)
+
+result += cnt1 * first
+result += cnt2 * second
 
 print(result)
