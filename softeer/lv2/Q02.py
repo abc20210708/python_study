dfc0c608782451d8dbc007bfaad0afda916039f3
abc20210708 <gotqlc2670@naver.com>
2023-02-15 17:25:40 +0900

    Update Q02.py

--- softeer/lv2/Q02.py
+++ softeer/lv2/Q02.py
@@ -16,3 +16,16 @@ elif d == desc:
     print("descending")
 else:
     print("mixed")
+    
+
+'''
+참고 블로그 https://dev-note-97.tistory.com/14
+
+✔ sort(reverse = True), sorted(reverse = True) / reverse(), reversed() 차이
+reverse = true 옵션을 사용한 sort 와 sorted는 
+오름차순 정렬 후 역순, 즉 "내림차순" 으로 정렬되는 것이고,
+
+reverse() 와 reversed() 는 
+정렬 과정 없이 순수 배열을 반대로 뒤집는 것입니다.
+
+'''
