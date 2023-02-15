6f0e3b72b2091c54e540a6d6f6ddf70d68b5897a
abc20210708 <gotqlc2670@naver.com>
2023-02-15 18:52:17 +0900

    join 함수 예제

--- hello.py
+++ hello.py
@@ -1,49 +1,8 @@
 
-# 장애물 인식 프로그램 다시 풀기
+# join 함수 예제
 
-import sys
+arr = ['a', 'b', 'c'] 
 
-def dfs(x, y):
-    if x <= -1 or y <= -1 or x >= n or y >= n:
-        return False
-    # 노드를 방문하지 않았다면
-    if block[x][y] == 1:
-        cnt.append(1)
-        
-        block[x][y] = 0
-        dfs(x - 1, y)
-        dfs(x + 1, y)
-        dfs(x, y - 1)
-        dfs(x, y + 1)
-        return True
-    return False
-
-
-# 지도의 크기 N
-n = 7
-
-block = [
-    [1,1,1,0,1,1,1],
-    [0,1,1,0,1,0,1],
-    [0,1,1,0,1,0,1],
-    [0,0,0,0,1,0,0],
-    [0,1,1,0,0,0,0],
-    [0,1,1,1,1,1,0],
-    [0,1,1,0,0,0,0],
-    ]
-# 자료 입력
-#for i in range(n):
-#   block.append(list(map(int, input())))
-
-cnt = []
-result_list = []
-for i in range(n):
-    for j in range(n):
-        if dfs(i, j) == True:
-            result_list.append(len(cnt))
-            cnt = []
-
-print(len(result_list))
-result_list.sort()
-for i in result_list:
-    print(i)
+print(' '.join(arr))
+print(''.join(arr))
+print(' + '.join(arr))
