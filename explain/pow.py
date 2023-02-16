af820dc6ba8b8732e0336db08c4b18291f1ab944
abc20210708 <gotqlc2670@naver.com>
2023-02-16 15:08:14 +0900

    Create pow.py

---
+++ explain/pow.py
@@ -0,0 +1,13 @@
+# pow 함수 예제
+
+# 참고 블로그 https://codechacha.com/ko/python-pow/
+
+'''
+# pow(x, y, z)로 제곱 계산
+
+pow(2, 3, 3)는 2의 3제곱을 3으로 나눈 나머지입니다.
+print(pow(2,3)) % 3와 동일한 결과를 리턴합니다.
+'''
+
+print(pow(2, 3, 3)) 
+print(pow(2, 3) % 3)
