72914c72e38f71c9cd21e618dcaedd1ce6448bb3
abc20210708 <gotqlc2670@naver.com>
2023-02-19 13:46:29 +0900

    문자열 연산하기

---
+++ explain/string.py
@@ -0,0 +1,14 @@
+# 문자열 연산하기
+
+# 1. 문자열 더해서 연결하기
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+
+# 2. 문자열 곱하기
+a = "python"
+print(a * 4)
+
+# 3. 문자열 길이 구하기
+b = "Life is too short"
+print(len(b))
