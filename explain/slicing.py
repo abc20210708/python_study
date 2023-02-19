bf676b97dcc5b68f381c7f5e88c8e809b52c8c6d
abc20210708 <gotqlc2670@naver.com>
2023-02-19 13:58:17 +0900

    문자열 슬라이싱

---
+++ explain/slicing.py
@@ -0,0 +1,14 @@
+# 문자열 슬라이싱
+a = "Life is too short, You need Python"
+#
+# b = a[0] + a[1] + a[2] + a[3]
+#print(a[0:4])
+#print(a[19:])
+#print(a[19:-7])
+
+# Pithon 문자열을 Python으로 바꾸러면?
+c = "Pithon"
+#print(c[:1])
+#print(c[2:])
+c = c[:1] + 'y' + c[2:]
+print(c)
