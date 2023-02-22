4d6e219300f5ee8c52f14b69d89b6a9f2632c0bb
abc20210708 <gotqlc2670@naver.com>
2023-02-22 15:06:38 +0900

    Update Q2693.py

--- baekjoon/basic/Q2693.py
+++ baekjoon/basic/Q2693.py
@@ -1,5 +1,12 @@
 # N번째 큰 수 
 
+n = int(input())
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    nums.sort()
+    print(nums[-3])
+
+'''
 arr = []
 
 for _ in range(int(input())):
@@ -9,3 +16,4 @@ for _ in range(int(input())):
     
 for i in arr:
     print(i)
+'''
