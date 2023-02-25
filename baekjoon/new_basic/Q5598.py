5e63086008888285e5657791f7178e581679654b
abc20210708 <gotqlc2670@naver.com>
2023-02-25 17:20:33 +0900

    Update Q5598.py

--- baekjoon/new_basic/Q5598.py
+++ baekjoon/new_basic/Q5598.py
@@ -2,9 +2,21 @@
 
 target = input()
 
+     
+x = input()
+y = ''
+for i in x :
+    if i in "ABC":
+        y += chr(ord(i) + 23)
+    else:
+        y += chr(ord(i) - 3)
+        
+print(y)
+
+'''
 for i in target:
     if ord(i) >= 65 and ord(i) <= 67:
         print(chr(ord(i)+23), end="")
     else:
         print(chr(ord(i)-3), end="")
-        
+'''
