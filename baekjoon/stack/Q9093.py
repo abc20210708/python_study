009b1a43dea8456bbf32647a459f252637cceb3c
abc20210708 <gotqlc2670@naver.com>
2023-03-01 17:39:47 +0900

    Update Q9093.py

--- baekjoon/stack/Q9093.py
+++ baekjoon/stack/Q9093.py
@@ -18,6 +18,19 @@ for i in range(n):
 ##[9,8,7,6,5,4,3,2,1,0]
 
 '''
+import sys
+ 
+n = int(sys.stdin.readline())
+ 
+for _ in range(n):
+    words = sys.stdin.readline().rstrip().split()
+
+    for word in words:
+        print(word[::-1], end=' ')
+    print()
+'''
+
+'''
 for _ in range(n):
     temp = input()
     temp += " "
