4a6083f57a13d386eba8f1e3ce20eb293d197b4f
abc20210708 <gotqlc2670@naver.com>
2023-03-01 14:10:04 +0900

    Update Qnew_id.py

--- baekjoon/new_basic/Qnew_id.py
+++ baekjoon/new_basic/Qnew_id.py
@@ -5,6 +5,7 @@ import re
 # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
 new_id = "...!@88BaT#*..y.abcdefghijklm-._"
 new_id = new_id.lower()
+print(new_id[-1])
 
 # 2단계 new_id에서 
 # 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를
@@ -26,17 +27,30 @@ for _ in new_id:
 
 if new_id[0:1] == ".":
     new_id = new_id[1:]
-elif new_id[-2:-1]:
+elif new_id[-2:-1] == ".":
     new_id = new_id[:-2]
 
 # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+
+if len(new_id) == 0:
+    new_id = "a"
 
 # 6단계 new_id의 길이가 16자 이상이면, new_id의 
 # 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
 # 만약 제거 후 마침표(.)가 new_id의 
 # 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 
+if len(new_id) >= 16:
+    new_id = new_id[15:]
+    
+if new_id[-2:-1] == ".":
+    new_id = new_id[:-2]
 
 # 7단계 new_id의 길이가 2자 이하라면, 
 # new_id의 마지막 문자를 new_id의 
 # 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+
+if len(new_id) <= 2:
+    new_id = new_id * 3
+    
+new_id = new_id[0:3]
