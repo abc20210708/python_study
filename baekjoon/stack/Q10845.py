31803f66c3bb7ba412dc716991039fe098af00fb
abc20210708 <gotqlc2670@naver.com>
2023-03-01 16:17:07 +0900

    Update Q10845.py

--- baekjoon/stack/Q10845.py
+++ baekjoon/stack/Q10845.py
@@ -15,33 +15,29 @@ back: 큐의 가장 뒤에 있는 정수를 출력한다.
 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다
 '''
 
-from collections import deque
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-queue = deque() 
+queue = list() 
    
 for i in range(n):
     temp = input().split()
     
     if temp[0] == 'push':
-        queue.insert(0, temp[1])
+        queue.append(int(temp[1]))
     elif temp[0] == 'pop':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue.pop())
+        if not queue : print(-1)
+        else : print(queue.pop(0))
     elif temp[0] == 'size':
         print(len(queue))
     elif temp[0] == 'empty':
-        if len(queue) == 0 : print(1)
-        else:
-            print(0)
+        if not queue : print(1)
+        else : print(0)
     elif temp[0] == 'front':
-        if len(queue) == 0 : print(-1)
-        else: print(queue[len(queue)-1])
-    elif temp[0] == "back":
-        if len(queue) == 0: print(-1)
+        if not queue : print(-1)
         else: print(queue[0])
+    elif temp[0] == "back":
+        if not queue: print(-1)
+        else: print(queue[-1])
