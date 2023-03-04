e94317173c43c48a9e9d8a80ef83ffc01ad1c154
abc20210708 <gotqlc2670@naver.com>
2023-03-04 18:25:52 +0900

    프린터

---
+++ baekjoon/stack/printer.py
@@ -0,0 +1,18 @@
+# 프린터
+def solution(p, location):
+    result = 0
+    
+    while len(p) > 0:
+        if p[0] == max(p):
+            p.pop(0)
+            result += 1
+            if location == 0:
+                break
+        else:
+            p.append(p.pop(0))
+        location = location -1 if location > 0 else len(p) -1
+        
+    return result
+            
+            
+print(solution([1, 1, 9, 1, 1, 1], 0))
