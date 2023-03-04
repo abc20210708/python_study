f677b8ddd9422ceaacaeffb07ecd5db583c2f10d
abc20210708 <gotqlc2670@naver.com>
2023-03-04 18:35:00 +0900

    Update printer.py

--- baekjoon/stack/printer.py
+++ baekjoon/stack/printer.py
@@ -15,4 +15,5 @@ def solution(p, location):
     return result
             
             
-print(solution([1, 1, 9, 1, 1, 1], 0))
+print(solution([2, 1, 3, 2], 2))
+# print(solution([1, 1, 9, 1, 1, 1], 0))
