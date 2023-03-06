8f0e674a35a8a908bd474719fc656fd8d7bdb5b3
abc20210708 <gotqlc2670@naver.com>
2023-03-06 15:06:45 +0900

    Update Q11655.py

--- baekjoon/string/Q11655.py
+++ baekjoon/string/Q11655.py
@@ -1,8 +1,23 @@
 # ROT13 
 
+#import sys
+#target = sys.stdin.readline()
 target = "One is 1"
 temp = ""
 
+for i in target:
+    if 'a' <= i <= 'z':
+        temp += chr((ord(i) + 13) if i <= 'm' else ord(i) - 13)
+    elif 'A' <= i <= 'Z':
+        temp += chr((ord(i) + 13) if i <= 'M' else ord(i) - 13)
+    else:
+        temp += i
+        
+print(temp)
+        
+
+
+'''
 for i in target:
     if i == " ":
         print(" ", end="")
@@ -18,3 +33,4 @@ for i in target:
         print(chr(temp), end="")
     elif ord(i) >= 48 and 57 >= ord(i):
         print(i, end="")
+'''
