c5ebf36f38eaac6996b538a20340653f29d106e1
abc20210708 <gotqlc2670@naver.com>
2023-03-07 18:01:32 +0900

    Update printer.py

--- baekjoon/stack/printer.py
+++ baekjoon/stack/printer.py
@@ -1,21 +1,26 @@
 # 프린터
-'''
+
+## 참고 블로그 https://hwisaek.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%94%84%EB%A6%B0%ED%84%B0-Python
 def solution(p, location):
     result = 0
     
     while len(p) > 0:
-        if p[0] == max(p):
+        if p[0] == max(p): # 우선순위 확인
+            # 맨 앞 문서 인쇄
             p.pop(0)
             result += 1
-            if location == 0:
+            if location == 0: # 요청한 문서이면 종료
                 break
         else:
+            # 맨 앞 문서를 대기 목록의 가장 마지막에 추가
             p.append(p.pop(0))
+        # 요청 문서 위치 변경
         location = location -1 if location > 0 else len(p) -1
         
     return result
-'''
 
+
+'''
 from collections import deque
 
 def solution(p, location):
@@ -37,6 +42,6 @@ def solution(p, location):
                 break
     return res
         
-            
+'''            
 print(solution([2, 1, 3, 2], 2))
 # print(solution([1, 1, 9, 1, 1, 1], 0))
