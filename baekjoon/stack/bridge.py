6a5efaca31a90bcee0c40199c19939614aecca2b
abc20210708 <gotqlc2670@naver.com>
2023-03-07 15:34:45 +0900

    Update bridge.py

--- baekjoon/stack/bridge.py
+++ baekjoon/stack/bridge.py
@@ -27,29 +27,29 @@ weight를 넘지 않아야 합니다.
 from collections import deque
 
 
-def solution(bridge_length, weight, truck_weights):
-    q = deque([0] * bridge_length)
+def solution(bl, weight, tw):
+    q = deque([0] * bl)
     
-    total_time = 0 # 걸리는 total 시간
+    cnt = 0 # 걸리는 total 시간
     qSum = 0 # 다리 weight
     
-    while truck_weights:
+    while tw:
         qSum -= q.popleft() # 빠져나온 트럭 무게 제거
         # 트럭은 1씩 움직이므로, 
         # while문이 돌아가는 시간 만큼 time 추가
-        total_time += 1 
+        cnt += 1 
         
         # 현재 다리 weight + 다음 트럭 무게가 weight를 넘으면
-        if (qSum + truck_weights[0]) > weight:
+        if (qSum + tw[0]) > weight:
             q.append(0) # 다리에 다음 트럭 올라갈 수 없으므로
             # 0을 추가
         
         else: #다음 트럭이 올라갈 수 있으면
-            next = truck_weights.pop(0) #다음 트럭 나옴
+            next = tw.pop(0) #다음 트럭 나옴
             qSum += next # 다음 트럭 무게 추가
             q.append(next) # 다음 트럭 queue에 추가
     
-    return total_time + len(q)
+    return cnt + len(q)
     # 다리를 빠져오지 못한 트럭 + total 시간
     
 print(solution(2, 10, [7,4,5,6]))
