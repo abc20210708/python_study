21acf76ce53ac7d44bdd450858d3b3b82810f823
abc20210708 <gotqlc2670@naver.com>
2023-03-07 13:25:33 +0900

    보물 다시 풀기

--- hello.py
+++ hello.py
@@ -1,27 +1,20 @@
-# 다리를 지나는 트럭 다시 풀기
+# 보물 다시 풀기
 
-from collections import deque
+'''
+5
+1 1 1 6 0
+2 7 8 3 1
+'''
 
-def solution(bl, w, tw):
-    q = deque([0] * bl)
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+s = 0
+
+for i in range(n):
+    s += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
     
-    total_time = 0
-    qSum = 0
-    
-    while tw:
-        # 빠져 나온 트럭 무게 제거
-        qSum -= q.popleft()
-        total_time += 1
-        
-        # 현재 다리 무게 + 다음 트럭 무게가 weight를 넘으면
-        if(qSum + tw[0]) > w:
-            q.append(0)
-        else:
-            next = tw.pop(0)
-            qSum += next
-            q.append(next)
-            
-    # 다리를 빠져오지 못한 트럭 + total 시간
-    return total_time + len(q)
-
-print(solution(2, 10, [7,4,5,6]))
+print(s)
