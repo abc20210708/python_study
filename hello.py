07e8207412e810d76960ade91a52de212700b557
abc20210708 <gotqlc2670@naver.com>
2023-03-07 14:53:05 +0900

    거스름돈 다시 풀기

--- hello.py
+++ hello.py
@@ -1,29 +1,14 @@
-# 캠핑 다시 풀기
+# 거스름돈 다시 풀기
 
-'''
-연속하는 20일 중 10일 사용
-28일 휴가
+coins = [500, 100, 50, 10, 5, 1]
+n = 1000 - 380
+cnt = 0
 
-연속하는 P일 중, L일 동안 사용 가능
-V일짜리 휴가 시작,  최대 며칠동안 사용가능한가?
-
-5 8 20
-5 8 17
-0 0 0
-
-Case 1: 14
-Case 2: 11
-
-'''
-
-i = 0
-
-while True:
-    i += 1
-    l, p, v = map(int, input().split())
-    if l + p + v == 0: break
-    num = ((v //p) * l) + min(l, v % p)
-    print(f'Case {i}:', num)
+for i in coins:
+    cnt += (n // i)
+    n %= i
+    
+print(cnt)
     
     
     
