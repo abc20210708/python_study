d22f3cf04f08bebbcc146ae9fc17da5929ce89bf
abc20210708 <gotqlc2670@naver.com>
2023-03-07 13:40:52 +0900

    Update Q2875.py

--- greedy/question/Q2875.py
+++ greedy/question/Q2875.py
@@ -2,11 +2,11 @@
 # 대회 or 인턴
 # 참고 블로그 https://jeongminhee99.tistory.com/65
 
-n, m, k = map(int, input().split())
-##n, m, k = 6, 3, 2
- 
-team = 0
 
+##n, m, k = 6, 3, 2
+
+'''
+team = 0
 
 while True:
     
@@ -19,11 +19,14 @@ while True:
     team += 1
     
 print(team)
-    
+'''  
 
 
-'''
-다른 풀이 
+
+# 6 10 3
+n, m, k = map(int, input().split())
+
+result = 0
 
 while n >= 2 and m >= 1 and n + m >= k + 3 :
     #2명, 1명 팀 만들 수 있고, 인텁쉽도 보낼 수 있는 경우
@@ -32,4 +35,4 @@ while n >= 2 and m >= 1 and n + m >= k + 3 :
     result += 1 #팀 수는 더해주기
     
 print(result)
-'''
+
