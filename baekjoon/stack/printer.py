98ffe6b4b35a2ac062a2dfc82f893fdf19be6c04
abc20210708 <gotqlc2670@naver.com>
2023-03-07 17:55:50 +0900

    Update printer.py

--- baekjoon/stack/printer.py
+++ baekjoon/stack/printer.py
@@ -1,4 +1,5 @@
 # 프린터
+'''
 def solution(p, location):
     result = 0
     
@@ -13,7 +14,29 @@ def solution(p, location):
         location = location -1 if location > 0 else len(p) -1
         
     return result
-            
+'''
+
+from collections import deque
+
+def solution(p, location):
+    queue = deque(p)
+    res = 0
+    
+    while queue:
+        first = max(queue)
+        cur = queue.popleft()
+        location -= 1
+        
+        if first > cur:
+            queue.append(cur)
+            if location == -1:
+                location = len(queue)-1
+        else:
+            res += 1
+            if location == -1:
+                break
+    return res
+        
             
 print(solution([2, 1, 3, 2], 2))
 # print(solution([1, 1, 9, 1, 1, 1], 0))
