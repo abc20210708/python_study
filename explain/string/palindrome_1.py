33f2a1a14194292db44d9000836773c4740ccd59
abc20210708 <gotqlc2670@naver.com>
2023-03-10 13:00:40 +0900

    정규식

---
+++ explain/string/palindrome_1.py
@@ -0,0 +1,34 @@
+'''
+슬라이싱
+
+s = ["가나다다나가"]
+
+if s == s[::-1]:
+    print("회문이다")
+
+'''
+import re
+
+def inPalindrome(str):
+    # 소문자로 변경
+    str = str.lower()
+    
+    # 정규식 사용
+    str = re.sub('[^a-z0-9]', '', str)
+    if str == str[::-1]:
+        print("회문입니다")
+    else:
+        print("회문이 아닙니다")
+        
+a = '0Madam, I\'m Adam0'
+inPalindrome(a)
+    
+'''
+re.sub('패턴', '바꿀문자열', '적용할문자열')
+ex_
+
+  print( re.sub('[0-9]+', 'n', '1 2 Fizz 4 Buzz Fizz 7 8') ) # 숫자만 찾아서 n으로 바꿈
+
+ 
+  'n n Fizz n Buzz Fizz n n'
+'''
