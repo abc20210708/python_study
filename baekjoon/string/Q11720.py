b8349a3687a4b22e53a8ca64748a8747d70df86e
abc20210708 <gotqlc2670@naver.com>
2023-03-12 13:25:12 +0900

    Update Q11720.py

--- baekjoon/string/Q11720.py
+++ baekjoon/string/Q11720.py
@@ -1,12 +1,17 @@
+# 숫자의 합
 
 #n = input()
 #m = input()
 
 n = "11"
-m = "10987654321"
+#m = "10987654321"
+m = list(map(int, input()))
+print(sum(m))
 
+'''
 total = 0
 for i in m:
     total += int(i)
     
 print(total)
+'''
