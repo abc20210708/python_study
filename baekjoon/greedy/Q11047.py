9b1f425404c98822f966ccbaf9be1c975258a3eb
abc20210708 <gotqlc2670@naver.com>
2023-03-13 18:11:26 +0900

    Update Q11047.py

--- baekjoon/greedy/Q11047.py
+++ baekjoon/greedy/Q11047.py
@@ -34,9 +34,10 @@ coins.sort(reverse=True)
 answer = 0
 for coin in coins:
     if K >= coin:
-        answer += K // coin 몫만큼 더하기
-        K %= coin 나머지 할당
+        answer += K // #coin 몫만큼 더하기
+        K %= coin #나머지 할당
         if K <= 0: # 만약 K가 0이면 반복문을 탈출합니다.
        		break
 print(answer) # 거슬러 준 동전 + 화폐의 수
+
 '''
