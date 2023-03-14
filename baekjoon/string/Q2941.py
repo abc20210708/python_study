1e7ce3589ee384fd651c6d2051013c331cb24be2
abc20210708 <gotqlc2670@naver.com>
2023-03-14 18:22:04 +0900

    크로아티아 알파벳

--- baekjoon/string/Q2941.py
+++ baekjoon/string/Q2941.py
@@ -1,8 +1,10 @@
 # 크로아티아 알파벳
 
-n = input()
+n = "ljes=njak"
 cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 for i in cro:
     n = n.replace(i, 'a')
 
 print(len(n))
+
+## 참고 블로그 https://one-hour.tistory.com/25
