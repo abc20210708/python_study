f232dba48d9b697a0df59a0052e5df4e1fe4de51
abc20210708 <gotqlc2670@naver.com>
2023-03-21 14:15:03 +0900

    Update Q04.py

--- appnedix/ex/Q04.py
+++ appnedix/ex/Q04.py
@@ -10,8 +10,24 @@
 s = "ThIs iS a TeSt"
 
 temp = s.split()
-print(temp)
+# print(temp)
 
 for i in temp:
     i = i[:1].upper() + i[1:].lower()
     print(i, end=" ")
+
+'''
+1. 문자열을 모두 소문자로 변환합니다.
+2. 단어별로 문자열을 분리합니다.
+3. 분리된 단어의 첫 글자를 대문자로 변환합니다.
+4. 나머지 글자는 모두 소문자로 변환합니다.
+5. 변환된 단어들을 다시 결합하여 최종 문자열을 반환합니다.
+
+'''    
+
+def solution(sentence):
+    words = sentence.lower().split() # 1, 2
+    capitalized_words = [word.capitalize() for word in words] # 3, 4
+    return " ".join(capitalized_words)
+
+print(solution(s))
