8f3d7996efe07abdb4059c3f8bcfa4773d64ec2e
abc20210708 <gotqlc2670@naver.com>
2023-03-22 22:06:27 +0900

    정렬의 기준 잡기

---
+++ gilbut/chap07/Q01.py
@@ -0,0 +1,26 @@
+## 정렬의 기준 잡기
+
+'''
+key : 정렬 기준을 정하는 함수를 기반으로 정렬
+
+reverse: Bool 값을 받아 최종 결과에서 
+        오름차순/내림차순의 여부 결정
+'''
+
+sample = [[0, 4], [0, 2], [1, 3], [2, 1]]
+
+def second(x):
+    return x[1]
+
+print(sorted(sample, key=second))
+print(sorted(sample, key=lambda x: x[1]))
+
+'''
+정렬 함수 규칙
+'''
+
+students = [
+    ('kim', 'B+', 18),
+    ('lee', 'A+', 21),
+    ('jeong', 'A', 18),
+]
