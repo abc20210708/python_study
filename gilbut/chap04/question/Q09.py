51a09636f05e229a9ddec81d4fde0946e43b5e46
abc20210708 <gotqlc2670@naver.com>
2023-03-27 17:09:45 +0900

    09. 짝지어 제거하기

---
+++ gilbut/chap04/question/Q09.py
@@ -0,0 +1,13 @@
+## 짝지어 제거하기
+
+'''
+순차적으로 입력을 받으면서 알파벳 쌍이 만들어지면
+해당 부분을 먼저 제거하는 방식으로 접근
+'''
+
+def explain(s):
+    stack = []
+    for case in s:
+        if stack and stack[-1] == case: stack.pop()
+        else: stack.append(case)
+    return 0 if stack else 1
