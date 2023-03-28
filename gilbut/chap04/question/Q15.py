41fa0e51ff375d8faeb202d1bd24fbba6790f79a
abc20210708 <gotqlc2670@naver.com>
2023-03-28 16:21:17 +0900

    Update Q15.py

--- gilbut/chap04/question/Q15.py
+++ gilbut/chap04/question/Q15.py
@@ -1,4 +1,12 @@
 ## 핸드폰 번호 가리기
+import re
+
+def explain(phone_number):
+    return re.sub('\d(?=\d{4})', '*', phone_number)
+# \d는 해당하는 모든 숫자들을 검색하고, 
+# 검색된 4글자를 제외해서 글자 길이에 상관없이
+# 정확히 나머지를 *로 치환
+
 
 def solution(phone_number):
     res = '*' * (len(phone_number) - 4)
