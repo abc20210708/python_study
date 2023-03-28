ca53e98e700d15a90785d428ca524a685b3df696
abc20210708 <gotqlc2670@naver.com>
2023-03-28 15:19:20 +0900

    Update Q14.py

--- gilbut/chap04/question/Q14.py
+++ gilbut/chap04/question/Q14.py
@@ -1,4 +1,12 @@
 # 문자열 다루기 기본
+import re
+
+def explain(s):
+    return len(s) in {4, 6} and bool(re.match('^[0-9]*$', s))
+
+# 숫자만 있는지 ^[0-9]*$
+# 알파벳만 있는지 ^[a-zA-z]*$
+# 한글만 있는지 ^[가-힣]*$
 
 def solution(s):
     
