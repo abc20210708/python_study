47ce7e32b3c642080a0fe0320f3eaa8b4dfde2ec
abc20210708 <gotqlc2670@naver.com>
2023-03-28 13:57:25 +0900

    12. 이진 변환 반복하기

--- gilbut/chap04/question/Q12.py
+++ gilbut/chap04/question/Q12.py
@@ -1,2 +1,18 @@
 ## 이진 변환 반복하기
 
+def solution(s):
+    res = [0, 0]
+    temp = s
+    
+    while len(temp) != 1:
+        # 이진 변환
+        res[0] += 1
+        length = len(temp)
+        # 0 제거
+        temp = temp.replace("0", "")
+        res[1] += length - len(temp)
+        temp = bin(len(temp))[2:]
+    
+    return res
+    
+print(solution("0111010"))
