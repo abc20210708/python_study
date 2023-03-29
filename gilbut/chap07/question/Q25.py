497647099f94d666de1f2b6d1898e1efdff28e8f
abc20210708 <gotqlc2670@naver.com>
2023-03-29 17:42:14 +0900

    25. 두 개 뽑아서 더하기

---
+++ gilbut/chap07/question/Q25.py
@@ -0,0 +1,14 @@
+## 두 개 뽑아서 더하기
+
+def solution(numbers):
+    res = []
+    
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            res.append(numbers[i] + numbers[j])
+            
+    res = set(res)
+    res = list(res)
+    res.sort()
+    
+    return res
