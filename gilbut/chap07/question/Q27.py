3b31daed7f6a50dd146d0c5c2818f5e42dd84b52
abc20210708 <gotqlc2670@naver.com>
2023-03-29 19:14:27 +0900

    Update Q27.py

--- gilbut/chap07/question/Q27.py
+++ gilbut/chap07/question/Q27.py
@@ -1,5 +1,9 @@
 ## 문자열 내 마음대로 정렬하기
 
 def explain(strings, n):
+    # 두 문자열 합치기
     return sorted(sorted(strings), key=lambda x:x[n] + x)
 
+def new_solution(strings, n):
+    # 튜플로 만들기
+    return sorted(strings, key=lambda x:(x[n], x))
