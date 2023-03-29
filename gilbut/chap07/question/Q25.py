834b8bf2c243cb9754e9ad41fe39fd4ef2857e7e
abc20210708 <gotqlc2670@naver.com>
2023-03-29 17:44:34 +0900

    Update Q25.py

--- gilbut/chap07/question/Q25.py
+++ gilbut/chap07/question/Q25.py
@@ -12,3 +12,12 @@ def solution(numbers):
     res.sort()
     
     return res
+
+def solution_1(numbers):
+    res = set()
+    
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            res.add(numbers[i] + numbers[j])
+
+    return sorted(res)
