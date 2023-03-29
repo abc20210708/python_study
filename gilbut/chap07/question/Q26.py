b78af31346798dbdda2bc1b69a0b4055c115ca22
abc20210708 <gotqlc2670@naver.com>
2023-03-29 18:59:48 +0900

    Update Q26.py

--- gilbut/chap07/question/Q26.py
+++ gilbut/chap07/question/Q26.py
@@ -1,8 +1,18 @@
 ## H-index
 
+# 오름차순 정렬
 def explain(citations):
     citations.sort()
     for idx, citation in enumerate(citations):
         if citation >= len(citations) - idx:
             return len(citations) - idx
     return 0
+
+#  내림차순 정렬
+def new_solution(citations):
+    citations.sort(reversed=True)
+    for idx, citation in enumerate(citations):
+        if idx >= citation:
+            return idx
+        
+    return len(citations)
