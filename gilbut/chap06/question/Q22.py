965e2cd0b8deb28f9e45d00db742c030974584ff
abc20210708 <gotqlc2670@naver.com>
2023-03-29 16:52:01 +0900

    22. 소수 찾기

---
+++ gilbut/chap06/question/Q22.py
@@ -0,0 +1,31 @@
+## 소수 찾기
+
+from itertools import permutations
+# 소수인지 판별하는 함수
+def checkPrime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def explain(numbers):
+    res = []
+    numbers = list(numbers)
+    num= []
+    
+    for i in range(1, len(numbers)+1):
+        num.append(list(permutations(numbers, i)))
+    # 2차원 배열
+    num = [int(''.join(y)) for x in num for y in x]
+    
+    for i in num:
+        if checkPrime(i):
+            res.append(i)
+    
+    return len(set(res))
+
+print(explain("17"))
