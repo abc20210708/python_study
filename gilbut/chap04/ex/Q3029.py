ac174e7a7f9d0abf057d6748409123b144948e12
abc20210708 <gotqlc2670@naver.com>
2023-03-30 16:27:47 +0900

    Update Q3029.py

--- gilbut/chap04/ex/Q3029.py
+++ gilbut/chap04/ex/Q3029.py
@@ -14,3 +14,31 @@ s = t % 60
 
 print("%02d:%02d:%02d" % (h, m, s))
     
+
+first = list(map(int, input().split(":")))
+second = list(map(int, input().split(":")))
+
+if first[2] > second[2]:
+    s = 60 - first[2] + second[2]
+    first[1] += 1
+else: 
+    s = second[2] - first[2]
+    
+if first[1] > second[1]:
+    m = 60 - first[1] + second[1]
+    first[0] += 1
+else:
+    m = second[1] - first[1]
+    
+if first[0] > second[0]:
+    h = 24 - first[0] + second[0]
+else:
+    h = second[0] - first[0]
+    
+if first[0] == second[0] and first[1] == second[1] and first[2] == second[2]:
+    h = 24
+    m = 0
+    s = 0
+    
+print("%02d:%02d:%02d" % (h, m, s))
+    
