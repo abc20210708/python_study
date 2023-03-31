4710ecf5c49cbb2b8b0190108b034ab24f8bc0a8
abc20210708 <gotqlc2670@naver.com>
2023-03-31 16:27:13 +0900

    Update Q1159.py

--- gilbut/chap04/ex/bronze/Q1159.py
+++ gilbut/chap04/ex/bronze/Q1159.py
@@ -1,5 +1,18 @@
 ## 농구 경기 (브론즈 2)
 
+li = sorted([input()[0] for _ in range(int(input()))])
+s = set(li)
+res = []
+
+for c in s:
+    if li.count(c) >= 5:
+        res.append(c)
+
+print(''.join(sorted(res)) if len(res) > 0 else "PREDAJA")
+
+
+
+
 n = int(input())
 arr = [0] * 26
 
