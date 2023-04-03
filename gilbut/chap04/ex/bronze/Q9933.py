bd18188393537331f5b1c4a14abddff5fcb71250
abc20210708 <gotqlc2670@naver.com>
2023-04-03 13:32:32 +0900

    Update Q9933.py

--- gilbut/chap04/ex/bronze/Q9933.py
+++ gilbut/chap04/ex/bronze/Q9933.py
@@ -4,11 +4,12 @@
 파일의 목록에 원래의 비밀번호,
 비밀번호를 역순으로 한 단어가 있다.
 '''
-
+# 참고 블로그 https://eboong.tistory.com/365
 n = int(input())
 words = [input() for _ in range(n)]
 
 for word in words:
     if word[::-1] in words:
+        #print(word)
         print(len(word), word[len(word) // 2])
         break
