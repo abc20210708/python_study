788e905fe841489286dfa633ac3dfc86db9d63ac
abc20210708 <gotqlc2670@naver.com>
2023-04-03 14:46:04 +0900

    Update Q14405.py

--- gilbut/chap04/ex/bronze/Q14405.py
+++ gilbut/chap04/ex/bronze/Q14405.py
@@ -4,3 +4,14 @@ import re
 
 res = re.fullmatch('(pi|ka|chu)+', input())
 print("YES" if res != None else "NO")
+
+import sys
+
+s = sys.stdin.readline().rstrip()
+
+tmp = re.sub('pi|ka|chu', "", s)
+
+if not tmp:
+    print("YES")
+else:
+    print("NO")
