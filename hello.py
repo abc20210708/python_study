e8d2f0ed1bc3055dd10054228c53a9a8f204b300
abc20210708 <gotqlc2670@naver.com>
2023-04-06 14:50:21 +0900

    파이썬 예제

--- hello.py
+++ hello.py
@@ -1,29 +1,16 @@
-# DFS 메서드 정의하기
+print(2 ** 4 + 11 % 5 - (20 / 2) + 13 // 2 * 3)
 
-def dfs(graph, v, visited):
-    # 현재 노드를 방문 처리
-    visited[v] = True
-    print(v, end=" ")
-    # 현재 노드와 연결된 다른 노드를 
-    # 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, v, visited)
-            
-graph = [
-    [],
-    [2, 3, 8], # 1
-    [1, 7],    # 2
-    [1, 4, 5], # 3
-    [3, 5],    # 4
-    [3, 4],    # 5
-    [7],       # 6
-    [2, 6, 8,],# 7
-    [1, 7]     # 8
-]
+n1 = int(input())
+n2 = int(input())
 
-# 각 노드가 방문된 정보를 리스트 자료형으로 표현 (1차원 리스트)
-visited = [False] * 9
+temp = 0
+if n1 > n2:
+    temp = n1
+else:
+    temp = n2
 
-# 정의된 DFS 함수 호출
-dfs(graph, 1, visited)
+if temp % 2 == 0:
+    print("더 큰 값인", temp, "은(는) 짝수입니다.")
+else:
+    print("더 큰 값인", temp, "은(는) 홀수입니다.")
+    
