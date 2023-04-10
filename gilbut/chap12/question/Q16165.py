aee31f3edaaf841c15b1391cc0e5428ec8518b3b
abc20210708 <gotqlc2670@naver.com>
2023-04-10 18:36:53 +0900

    16165. 걸그룹 마스터 준석이 (실버 3) *

---
+++ gilbut/chap12/question/Q16165.py
@@ -0,0 +1,26 @@
+# 걸그룹 마스터 준석이 (실버 3) *
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dic1, dic2 = {}, {}
+
+for _ in range(n):
+    team = input().rstrip()
+    members = []
+    for i in range(int(input())):
+        name = input().rstrip()
+        dic1[name] = team
+        members.append(name)
+    dic2[team] = members
+
+for _ in range(m):
+    target = input().rstrip()
+    num = int(input())
+    if num == 1:
+        print(dic1[target])
+    else:
+        mem = dic2[target]
+        print('\n'.join(sorted(mem)))
+
