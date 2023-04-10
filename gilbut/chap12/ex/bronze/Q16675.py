7a751cb5cf8f890047cb1782cb78e14be31f515b
abc20210708 <gotqlc2670@naver.com>
2023-04-10 20:19:50 +0900

    16675. 두 개의 손 (브론즈 2) *

---
+++ gilbut/chap12/ex/bronze/Q16675.py
@@ -0,0 +1,16 @@
+# 두 개의 손 (브론즈 2) *
+
+# S - 가위, R - 바위, P - 보, 무조건 이기는 방법
+
+ml, mr, tl, tr = ('SPR'.index(i) for i in input().split())
+'''
+받은 문자열을 가위(S), 바위(R), 보(P)를 
+각각 0, 1, 2로 변환하여 ml, mr, tl, tr 변수에 저장합니다.
+'''
+
+if ml == mr and (ml + 2)% 3 in [tl,tr]:
+    print("TK")
+elif tl == tr and (tl + 2)%3 in [ml,mr]:
+    print("MS")
+else:
+    print("?")
