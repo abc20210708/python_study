62df6c20646361826a8178e193455f70eab0b599
abc20210708 <gotqlc2670@naver.com>
2023-04-14 17:07:18 +0900

    민균이의 비밀번호 다시 풀기

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
-# 애너그램 다시 풀기
+# 민균이의 비밀번호 다시 풀기
 
-for _ in range(int(input())):
-    s1, s2 = map(str, input().split())
-    a1 = list(s1)
-    a2 = list(s2)
-    a1.sort()
-    a2.sort()
-    
-    if a1 == a2:
-        print(f"{s1} & {s2} are anagrams.")
-    else:
-        print(f"{s1} & {s2} are NOT anagrams.")
+n = int(input())
+arr = [input() for _ in range(n)]
+
+
+for i in arr:
+    if i[::-1] in arr:
+        print(len(i), i[len(i)//2])
+        break
     
