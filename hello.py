39467da8a0ef6e112f7248fa500cf2694154bf34
abc20210708 <gotqlc2670@naver.com>
2023-04-14 16:19:04 +0900

    애너그램 다시 풀기

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
-# 도비의 난독증 테스트 다시 풀기
+# 애너그램 다시 풀기
 
-while 1:
-    n = int(input())
-    if n == 0: break
-    arr = []
-    for _ in range(n):
-        s = input()
-        arr.append(s)
-    arr.sort(key=str.lower)
-    print(arr[0])
+for _ in range(int(input())):
+    s1, s2 = map(str, input().split())
+    a1 = list(s1)
+    a2 = list(s2)
+    a1.sort()
+    a2.sort()
+    
+    if a1 == a2:
+        print(f"{s1} & {s2} are anagrams.")
+    else:
+        print(f"{s1} & {s2} are NOT anagrams.")
+    
