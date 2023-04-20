9bc674b71e539a99d5f789c84a13b94b333a4274
abc20210708 <gotqlc2670@naver.com>
2023-04-20 19:13:38 +0900

    Update Q5_5534.py

--- gilbut/chap04/ex/silver/Q5_5534.py
+++ gilbut/chap04/ex/silver/Q5_5534.py
@@ -10,22 +10,26 @@ name이 각 입력 문자열(s)의 부분 문자열로 존재하는지
 import sys
 input = sys.stdin.readline
 
-def solution(a, b, name):
-    for i in a:
-        for j in [k for k in b if i < k]:
+def solution(start, second, name):
+    # 시작 위치와 두 번째 위치를 이용하여 새 간판을 만들 수 있는가
+    for i in start:
+        for j in [k for k in second if i < k]:
             step = j - i
+            # 일정한 간격으로 문자열을 확인해 새 간판을 만들 수 있는지
             if name in s[i::step]:
                 print(s[i::step])
-                return 1
-    return 0 
+                return 1 # 새 간판을 만들 수 있는 경우 1 반환
+    return 0  # 새 간판을 만들 수 없는 경우 0 반환
 
 n = int(input())
 name = input().rstrip()
-cnt = 0
+cnt = 0 # 상근이가 만들 수 있는 간판의 수를 세는 변수
 
 for _ in range(n):
     s = input().rstrip()
+    # 첫 번째 문자의 위치 찾기
     start = [i for i in range(len(s)) if s[i] == name[0]]
+    # 두 번째 문자의 위치 찾기
     second = [i for i in range(len(s)) if s[i] == name[1]]
     cnt += solution(start, second, name)
 
