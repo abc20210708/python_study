6197d1f284a2221f8356fd6d0577ebe69778588d
abc20210708 <gotqlc2670@naver.com>
2023-04-22 22:27:13 +0900

    Update Q2167.py

--- baekjoon/array/Q2167.py
+++ baekjoon/array/Q2167.py
@@ -1,17 +1,23 @@
 ## 2차원 배열의 합 (실버 5) *
 
-# 참고 블로그 https://blog.naver.com/parkjh9876/221867004374
-N, M = map(int, input().split())
-A = [list(map(int, input().split())) for _ in range(N)]
-K = int(input())
-Q = [tuple(map(int, input().split())) for _ in range(K)]
-dp = [[0] * (M + 1) for _ in range(N + 1)]
+# 참고 블로그 https://blog.naver.com/baeksh0330/222763534587
+# 2차원 배열의 합. 브론즈 1
+# dp를 이용하지 않으면 시간초과가 난다는 말을 들은거 같다. 입력받는 동시에 표를 만들어줘야 하나?
+n, m = map(int, input().split())
+arr=[]
+dp = [[0] * (m+1)for _ in range(n+1)]
+# input_data = [list(map(int, input('숫자 입력 : ').split())) for _ in range(n)] # 이렇게 한 줄로 줄일 수 있음
 
-for i in range(1, N + 1):
-    for j in range(1, M + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + A[i - 1][j - 1]
+for _ in range(n):
+    arr.append(list(map(int, input('숫자 입력 : ').split())))
 
-for i in Q:
-    (x1, y1, x2, y2) = i
-    result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
-    print(result)
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+k = int(input('숫자입력 k : '))
+
+for _ in range(k):
+    i, j, x, y = map(int, input('숫자 입력 : ').split())
+    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
+[출처] [파이썬/백준 2167] 2차원 배열의 합|작성자 baeksh0330
