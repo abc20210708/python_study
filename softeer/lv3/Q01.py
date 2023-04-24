b4e9fe4e9d91866540f6ab38a41e9b216c24c793
abc20210708 <gotqlc2670@naver.com>
2023-04-24 22:35:04 +0900

    softeer.lv3. 성적 평균 *

---
+++ softeer/lv3/Q01.py
@@ -0,0 +1,20 @@
+## 성적 평균 *
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+s = [0] + list(map(int, input().split()))
+result = []
+
+for i in range(k):
+    sum = 0
+    a, b = map(int, input().split())
+    x = s[a:b+1]
+
+    for j in x:
+        sum += j
+    result.append(round(sum/len(x), 2))
+
+for i in result:
+    print(format(i, ".2f")) # 참고 블로그 https://blockdmask.tistory.com/534
