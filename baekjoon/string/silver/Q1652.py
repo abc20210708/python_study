35f7e001276055f9a85f4b12f27432c5ce21651f
abc20210708 <gotqlc2670@naver.com>
2023-04-25 18:55:51 +0900

    1652. 누울 자리를 찾아라 (실버 5) *

---
+++ baekjoon/string/silver/Q1652.py
@@ -0,0 +1,32 @@
+## 누울 자리를 찾아라 (실버 5) *
+
+# 참고 블로그 https://chagokchagock.tistory.com/16
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = []
+w, h = 0, 0
+cnt = 0
+for i in range(n):
+    li.append(list(input().rstrip()))
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if li[i][j] == '.':
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == 2:
+            w += 1
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if li[j][i] == '.':
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == 2:
+            h += 1
+print(w, h)
