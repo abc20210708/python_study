f5f6b4d170b54fc4a63fb52218b1520d319393cb
abc20210708 <gotqlc2670@naver.com>
2023-04-26 17:03:29 +0900

    Update Q9251.py

--- baekjoon/string/gold/Q9251.py
+++ baekjoon/string/gold/Q9251.py
@@ -32,3 +32,19 @@ for i in range(l1):
 
 # 최종적으로 cache에 저장된 값들 중 가장 큰 값이 LCS의 길이이므로 출력
 print(max(cache)) 
+
+# 참고 블로그
+#https://velog.io/@piopiop/%EB%B0%B1%EC%A4%80-9251-LCS-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
+str_a = ' '+ sys.stdin.readline().rstrip()
+str_b = ' '+ sys.stdin.readline().rstrip()
+dp = [[0] * len(str_b) for _ in range(len(str_a))]
+
+for i in range(1, len(str_a)):
+    for j in range(1, len(str_b)):
+        if str_a[i] == str_b[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
