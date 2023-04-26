dfcc30f3c4e79e2fcbf3826a66014583efef87b4
abc20210708 <gotqlc2670@naver.com>
2023-04-26 21:26:11 +0900

    Update Q1244_1.py

--- implementation/question/silver/Q1244_1.py
+++ implementation/question/silver/Q1244_1.py
@@ -3,29 +3,36 @@
 # 참고 블로그
 # https://dogsavestheworld.tistory.com/entry/python-%EB%B0%B1%EC%A4%80-1244%EB%B2%88-%EC%8A%A4%EC%9C%84%EC%B9%98-%EC%BC%9C%EA%B3%A0-%EB%81%84%EA%B8%B0
 
-n = int(input())
-# idx 값 맞추기 위해 0번에 미지값 추가
-s = [-1] + list(map(int, input().split()))
-k = int(input())
-students = [tuple(map(int, input().split())) for _ in range(k)]
+# 입력값 받기
+n = int(input()) # 스위치의 개수
+# 스위치의 초기 상태, 0번 인덱스에 -1 추가
+s = [-1] + list(map(int, input().split())) 
+k = int(input()) # 학생 수
+# 학생들의 성별과 번호를 튜플 형태로 입력받음
+students = [tuple(map(int, input().split())) for _ in range(k)] 
 
+# 학생들의 조작에 따라 스위치 상태 변경
 for g, num in students:
-    # 성별이 남자일때
+    # 성별이 남자일 때
     if g == 1:
-        for i in range(num, n+1, num):
+        # 주어진 번호의 배수에 해당하는 스위치들의 상태를 반전시킴
+        for i in range(num, n+1, num): 
             s[i] = 1 - s[i]
-    # 성별이 여자일때
+    # 성별이 여자일 때
     else:
-        s[num] = 1 - s[num]
-        for k in range(n//2):
-            if num + k > n or num - k < 1: break
-            if s[num+k] == s[num-k]:
-                s[num+k] = 1 - s[num+k]
+        # 주어진 번호의 스위치 상태를 반전시킴
+        s[num] = 1 - s[num] 
+        # 해당 스위치를 중심으로 좌우로 대칭인 스위치들의 상태를 반전시킴
+        for k in range(n//2): 
+            if num + k > n or num - k < 1: break # 범위를 벗어나면 중단
+            if s[num+k] == s[num-k]: # 대칭인 스위치들의 상태가 같으면
+                s[num+k] = 1 - s[num+k] # 상태를 반전시킴
                 s[num-k] = 1 - s[num-k]
             else:
                 break  
 
+# 변경된 스위치 상태 출력
 for i in range(1, n+1):
-    print(s[i], end=' ')
-    if i % 20 == 0:
+    print(s[i], end=' ') # 스위치 상태 출력
+    if i % 20 == 0: # 20개씩 줄바꿈
         print()
