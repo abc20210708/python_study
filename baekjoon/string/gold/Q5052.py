cba8814cb1e814d9444f2bb6bfe356ec3161bb9c
abc20210708 <gotqlc2670@naver.com>
2023-04-26 19:03:11 +0900

    Update Q5052.py

--- baekjoon/string/gold/Q5052.py
+++ baekjoon/string/gold/Q5052.py
@@ -3,25 +3,23 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
+t = int(input()) # 테스트 케이스의 개수를 입력받음
 for _ in range(t):
-    n = int(input())
-    arr = [input().strip() for _ in range(n)]
-    arr.sort()
-    check = "yes"
-    
-    # 반복문을 통해 전화번호 확인
-    for i in range(len(arr) -1):
-        # 현재 전화번호의 문자열과 다음 전화번호의
-        # 현재 전화번호 길이만큼의 문자열과 같은지 확인
-        # 같으면 일관성이 없는 것
-        if arr[i] == arr[i+1][0:len(arr[i])]:
-            print(arr[i+1][0:len(arr[i])])
-            print(arr[i+1])
-            print(arr[0:len(arr[i])])
-            check = "no"
-    if check == "no":
-        print("NO")
-    else:
-        print("YES")        
+    n = int(input()) # 전화번호의 수를 입력받음
+    # 전화번호를 입력받아 배열에 저장
+    arr = [input().strip() for _ in range(n)] 
+    arr.sort() # 전화번호를 사전순으로 정렬
+    check = "yes" # 일관성 여부를 나타내는 변수, 처음에는 "yes"로 초기화
+
+# 반복문을 통해 전화번호를 확인
+for i in range(len(arr) - 1):
+    # 만약 현재 전화번호가 다음 전화번호의 접두어인 경우
+    if arr[i] == arr[i+1][0:len(arr[i])]:
+        check = "no" # 일관성이 없음을 표시
+        break # 반복문 종료
+        
+if check == "no":
+    print("NO") # 일관성이 없는 경우 "NO" 출력
+else:
+    print("YES") # 일관성이 있는 경우 "YES" 출력 
     
