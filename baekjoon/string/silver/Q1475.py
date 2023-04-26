cc9c82d1a7dc58439a3db5cd536ff32b5da10e44
abc20210708 <gotqlc2670@naver.com>
2023-04-26 17:14:22 +0900

    1475. 방 번호 (실버 5) *

---
+++ baekjoon/string/silver/Q1475.py
@@ -0,0 +1,20 @@
+## 방 번호 (실버 5) *
+
+import sys
+input = sys.stdin.readline().rstrip
+
+s = input()
+
+res = [0] * 10
+
+for i in range(len(s)):
+    num = int(s[i])
+    if num == 6 or num == 9:
+        if res[6] <= res[9]:
+            res[6] += 1
+        else:
+            res[9] += 1
+    else:
+        res[num] += 1
+        
+print(max(res))
