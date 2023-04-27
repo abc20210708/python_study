ea32cf8d6f371bcb68c61e89360ca3b638f7352e
abc20210708 <gotqlc2670@naver.com>
2023-04-27 17:12:44 +0900

    5430. AC (골드 5)*

---
+++ baekjoon/string/gold/Q5430.py
@@ -0,0 +1,41 @@
+## AC (골드 5)*
+
+# 참고 블로그 https://clap0107.tistory.com/16
+
+from collections import deque
+
+t = int(input())
+
+for _ in range(t):
+    p = input()
+    n = int(input())
+    x = input()[1:-1]
+    nums = x.split(',')
+
+    queue = deque(nums)
+
+    reverse = 0
+    flag = False
+
+    if n == 0:
+        queue = []
+
+    for i in p:
+        if i == 'R':
+            reverse += 1
+        else:
+            if len(queue) == 0:
+                print('error')
+                flag = True
+                break
+            elif reverse % 2 == 1:
+                queue.pop()
+            else:
+                queue.popleft()
+
+    if not flag:
+        if reverse % 2 == 1:
+            queue.reverse()
+        print('[' + ','.join(queue) + ']')
+                
+    
