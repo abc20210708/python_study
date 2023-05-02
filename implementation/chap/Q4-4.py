5173045accbd18d86b7d49575f64b65965a9a1b3
abc20210708 <gotqlc2670@naver.com>
2023-05-02 14:54:30 +0900

    Update Q4-4.py

--- implementation/chap/Q4-4.py
+++ implementation/chap/Q4-4.py
@@ -1,62 +1,63 @@
 ## 게임 개발
 
-# n, m을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
+# 참고 블로그 https://kk-programming.tistory.com/22
 
-# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
-d = [[0] * m for _ in range(n)]
-# 현재 캐릭터의 x좌표, y좌표, 방향을 입력 받기
-x, y, direction = map(int, input().split())
-d[x][y] = 1 # 현재 좌표 방문 처리
+n, m = map(int,input().split()) #n * m 크기의 맵
+#캐릭터 위치 (x,y), 바라보는 방향:d
+x, y, d = map(int,input().split()) 
 
-# 전체 맵 정보를 입력받기
-arr = []
+ 
+# 전체 map 정보 입력받기(육지:0, 바다:1)
+map_info = [] 
 for i in range(n):
-    arr.append(list(map(int, input().split())))
-
-# 북, 동, 남, 서 방향 정의
+  map_info.append(list(map(int,input().split())))
+ 
+# 방향 d: [0,1,2,3]에 따라 이동 방향 설정 (북,동,남,서)
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-
-# 왼쪽으로 회전
+ 
+# map_info: 방문한 위치를 저장하기 위한 맵, 0으로 초기화 
+check = [[0] * m for _ in range(n)]
+check[x][y] = 1
+ 
+# 왼쪽으로 회전시 방향 바뀌는 함수
 def turn_left():
-    global direction
-    direction -= 1
-    if direction == -1:
-        direction = 3
-        
-# 시뮬레이션 시작
-cnt = 1
-turn_time = 0
-while 1:
-    # 왼쪽으로 회전
-    turn_left()
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이둥
-    if d[nx][ny] == 0  and arr[nx][ny] == 0:
-        d[nx][ny] = 1
-        x = nx
-        y = ny
-        cnt += 1
-        turn_time = 0
-        continue
+  global d
+  d -= 1
+  if d == -1 : #0->-1 : 북 -> 서
+    d = 3
+ 
+count = 1 #방문한 칸의 수
+turn_num = 0 
+while True:
+  #왼쪽으로 회전
+  turn_left()
+ 
+  #회전한 이후 정면이 가보지 않은 칸이고, 육지이면 이동
+  nx = x + dx[d]
+  ny = y + dy[d]
+  
+  if check[nx][ny] == 0 and map_info[nx][ny] == 0:
+    check[nx][ny] = 1
+    x, y = nx, ny
+    count += 1 
+    turn_num = 0 
+    continue
+  # 회전한 이후 정면에 가본 칸이거나 바다인 경우 회전
+  else:
+    turn_num += 1 
     
-    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+  # 4방향 모두 가본 칸이거나 바다로 되어 있는 칸 (네방향 모두 갈 수 없는 칸) 
+  #-> 앞에 else문이 연달아 4번 나올 경우
+  if turn_num == 4: 
+    nx = x - dx[d]
+    ny = y - dy[d]
+    # 뒤로 갈 수 있다면(뒤에 있는 칸이 육지) 이동
+    if map_info[nx][ny]==0:
+      x, y = nx, ny
+    # 뒤가 바다로 막혀있다면 움직임을 멈춘다 -> 종료
     else:
-        turn_time += 1
-    # 네 방향 모두 갈 수 없는 경우
-    if turn_time == 4:
-        nx = x - dx[direction]
-        ny = y - dy[direction]
-        # 뒤로 갈 수 있다면 이동하기
-        if arr[nx][ny] == 0:
-            x = nx
-            y = ny
-        # 뒤에 바다로 막혀 있는 경우
-        else: 
-            break
-        turn_time = 0
-
-print(cnt)  
-        
+      break
+    turn_num = 0 # 방향 다시 순회
+ 
+print(count)
