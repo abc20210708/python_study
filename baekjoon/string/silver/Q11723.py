40e357434c4b166a1c3475a518d70b4128f06847
abc20210708 <gotqlc2670@naver.com>
2023-05-02 17:19:27 +0900

    Update Q11723.py

--- baekjoon/string/silver/Q11723.py
+++ baekjoon/string/silver/Q11723.py
@@ -26,3 +26,13 @@ for _ in range(int(input())):
                 arr.discard(n)
             else:
                 arr.add(n)
+                
+# 참고 블로그  https://hbase.tistory.com/111
+'''
+remove():
+이미 비어있는데 제거하려고 하면 KeyError 발생
+
+discard():
+비어있어도 제거할 때 에러가 발생하지 않는다.
+존재하지 않음을 보장하려고 할 때 사용
+'''
