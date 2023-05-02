ed867915c33264887db52ac35145c585125322b4
abc20210708 <gotqlc2670@naver.com>
2023-05-02 14:10:45 +0900

    예제 시각 다시 풀기

--- hello.py
+++ hello.py
@@ -1,23 +1,12 @@
-# 상하좌우 다시 풀기
+# 예제 시각 다시 풀기
 
-n = int(input())
-steps = list(map(str, input().split()))
+hour = int(input())
+cnt = 0
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move = ['L', 'R', 'U', 'D']
+for i in range(hour+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i)+str(j)+str(k):
+                cnt += 1
 
-x = 1
-y = 1
-
-for step in steps:
-    for i in range(len(move)):
-        if step == move[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    
-    x, y = nx, ny
-
-print(x, y) 
+print(cnt)
