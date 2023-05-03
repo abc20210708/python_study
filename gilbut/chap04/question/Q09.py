e54ffa5259ab26f01435c44f4db6d2910f45c540
abc20210708 <gotqlc2670@naver.com>
2023-05-03 15:14:24 +0900

    Update Q09.py

--- gilbut/chap04/question/Q09.py
+++ gilbut/chap04/question/Q09.py
@@ -7,9 +7,10 @@
 
 def explain(s):
     stack = []
-    for case in s:
-        if stack and stack[-1] == case: stack.pop()
-        else: stack.append(case)
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else: stack.append(i)
     return 0 if stack else 1
 
 print(explain("cdcd"))
