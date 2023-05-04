ea585cb325b66d671e35ac46106e01ef70dcb721
abc20210708 <gotqlc2670@naver.com>
2023-05-04 21:56:22 +0900

    Update Q4_3613.py

--- gilbut/chap04/ex/silver/Q4_3613.py
+++ gilbut/chap04/ex/silver/Q4_3613.py
@@ -1,18 +1,27 @@
 ## Java vs C++ (실버 3) *
 
 # 참고 블로그 http://mrk0607.tistory.com/767
-s = "long_and_mnemonic_identifier"
-#s = input()
+#s = "long_and_mnemonic_identifier"
+# 입력받은 문자열
+import sys
+s = sys.stdin.readline().rstrip()
+# 결과를 저장할 변수 초기화
 res = ''
 
+# '_'이 포함되어 있는 경우, C++ 형식인 경우
 if '_' in s:
     flag = 0
+    # '_'이 문자열의 첫 글자 또는 
+    # 마지막 글자에 위치하거나, '__'가 존재하면 에러
     if s[0] == '_' or s[-1] == '_' or '__' in s:
         res = "Error!"
     else:
         for i in s:
+            # 대문자가 포함된 경우 에러
             if i.isupper():
                 res = "Error!"
+                break
+            # '_'가 나타난 경우, 그 다음 문자를 대문자로 바꿔준다
             if i == "_":
                 flag = 1
                 continue
@@ -20,19 +29,24 @@ if '_' in s:
                 res += i.upper()
                 flag = 0
                 continue
+            # 그 외의 경우는 그대로 res에 추가한다
             res += i
+# '_'이 포함되어 있지 않은 경우, Java 형식인 경우
 else:
+    # 첫글자가 대문자인 경우 에러
     if s[0].isupper():
         res = "Error!"
     else:
         for i in s:
+            # 대문자가 나타난 경우 '_'를 추가하고, 소문자로 변환한다
             if i.isupper():
                 res += "_" + i.lower()
                 continue
+            # 그 외의 경우는 그대로 res에 추가한다
             res += i
 
+# 결과 출력
 print(res)
-
 '''
 먼저, 입력된 문자열 s에 ''가 있는 경우를 처리합니다. 
 이때, 문자열의 첫 번째 혹은 마지막 문자가 ''인 경우, 
