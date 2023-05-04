38c85f8c6b6157144a69b1a8921008986e615b25
abc20210708 <gotqlc2670@naver.com>
2023-05-04 20:30:27 +0900

    걸그룹 마스터 준석이(실버 3) 다시 풀기

--- hello.py
+++ hello.py
@@ -1,15 +1,25 @@
-# 방 번호(실버 5) 다시 풀기
+# 걸그룹 마스터 준석이(실버 3) 다시 풀기
 
-s = input()
-arr = [0] * 10
+n, m = map(int, input().split())
+dic1 = {}
+dic2 = {}
 
-for i in s:
-    if int(i) == 6 or int(i) == 9:
-        if arr[6] <= arr[9]:
-            arr[6] += 1
-        else:
-            arr[9] += 1
+for _ in range(n):
+    group = input()
+    tmp = []
+    for i in range(int(input())):
+        name = input()
+        dic1[name] = group
+        tmp.append(name)
+    tmp.sort()
+    dic2[group] = tmp
+
+for _ in range(m):
+    target = input()
+    num = int(input())
+    if num == 1:
+        print(dic1[target])
     else:
-        arr[int(i)] += 1
-
-print(max(arr))
+        temp = dic2.get(target)
+        for i in temp:
+            print(i)
