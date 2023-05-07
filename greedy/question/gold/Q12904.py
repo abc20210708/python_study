5c604a701b82a19f07fa14309db31de92db5ef5b
abc20210708 <gotqlc2670@naver.com>
2023-05-07 19:52:19 +0900

    12094. A와 B (골드 5) *

---
+++ greedy/question/gold/Q12904.py
@@ -0,0 +1,31 @@
+# A와 B (골드 5) *
+
+# 참고 블로그 https://chocochip101.tistory.com/entry/%EB%B0%B1%EC%A4%80-12904%EB%B2%88-%ED%8C%8C%EC%9D%B4%EC%8D%AC-A%EC%99%80-B
+
+import sys
+input = sys.stdin.readline
+
+s = list(input().rstrip())
+t = list(input().rstrip())
+
+
+'''
+1. 문자열 뒤에 A를 추가
+2. 문자열을 뒤집고, 뒤에 B를 추가
+'''
+
+chk = False
+while t:
+    if t[-1] == "A":
+        t.pop()
+    elif t[-1] == "B":
+        t.pop()
+        t.reverse()
+    if s == t:
+        chk = True
+        break
+    
+if chk:
+    print(1)
+else:
+    print(0)
