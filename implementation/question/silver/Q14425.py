004d049e396e5b952b73f3544993791c9f2c9b4a
abc20210708 <gotqlc2670@naver.com>
2023-05-10 18:04:01 +0900

    Update Q14425.py

--- implementation/question/silver/Q14425.py
+++ implementation/question/silver/Q14425.py
@@ -2,7 +2,7 @@
 
 import sys
 input = sys.stdin.readline
-
+'''
 n, m = map(int, input().split())
 s = set([input() for _ in range(n)])
 cnt = 0
@@ -12,3 +12,22 @@ for _ in range(m):
     if t in s:
         cnt += 1
 print(cnt)
+'''
+## 다른 풀이 dictionary 사용
+from itertools import count
+
+# dictionary 사용
+d = {}
+count = 0
+n1, n2 = map(int, input().split())
+
+for _ in range(n1):
+    data = input().strip()
+    d[data] = True
+
+for _ in range(n2):
+    data = input().rstrip()
+    if data in d:
+        count += 1
+
+print(count)
