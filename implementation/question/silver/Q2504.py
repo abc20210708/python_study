b0881d9e7311b1b70937dc9a72df916629ff0783
abc20210708 <gotqlc2670@naver.com>
2023-05-10 19:01:56 +0900

    2504. 괄호의 값 (실버 1)*

---
+++ implementation/question/silver/Q2504.py
@@ -0,0 +1,46 @@
+## 괄호의 값 (실버 1)*
+# 참고 블로그 https://www.jongung.com/283
+
+import sys
+
+input = sys.stdin.readline().rstrip()
+
+# (()[[]])([])
+
+# [][]((])
+list = []
+chk = True
+val = 1
+res = 0
+
+for i, str in enumerate(input):
+    if str == "(":
+        list.append(str)
+        val *= 2
+
+    elif str == "[":
+        list.append(str)
+        val *= 3
+
+    elif str == ")":
+        if not list or list[-1] == "[":
+            chk = False
+            break
+        if input[i-1] == "(":
+            res += val
+        list.pop()
+        val //= 2
+
+    elif str == "]":
+        if not list or list[-1] == "(":
+            chk = False
+            break
+        if input[i-1] == "[":
+            res += val
+        list.pop()
+        val //= 3
+
+if not chk or list:
+    print(0)
+else:
+    print(res)
