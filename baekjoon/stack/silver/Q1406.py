7a9ed446633039c1735e6536090cddc650397c74
abc20210708 <gotqlc2670@naver.com>
2023-05-28 15:40:51 +0900

    에디터 (실버 2) *

---
+++ baekjoon/stack/silver/Q1406.py
@@ -0,0 +1,31 @@
+## 에디터 (실버 2) *
+# 참고 코드 https://www.acmicpc.net/submit/1406/61395524
+
+from sys import stdin
+
+st1 = list(stdin.readline().rstrip())
+st2 = []
+n = int(input())
+
+for _ in range(n):  # 입력 횟수(n)만큼 반복합니다.
+    i = stdin.readline().rstrip()  # 표준 입력에서 한 줄씩 입력을 받아옵니다.
+
+    if i[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+        else:
+            continue
+    elif i[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+        else:
+            continue
+    elif i[0] == 'B':
+        if st1:
+            st1.pop()
+        else:
+            continue
+    elif i[0] == 'P':
+        st1.append(i[2])
+
+print(''.join(st1 + list(reversed(st2))))
