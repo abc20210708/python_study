6e3740970bef501ce2c66b45cc779064f55f9563
abc20210708 <gotqlc2670@naver.com>
2023-05-30 20:58:03 +0900

    Update Q5-7.py

--- dfs_bfs/chap/Q5-7.py
+++ dfs_bfs/chap/Q5-7.py
@@ -1,11 +1,11 @@
 # DFS 메서드 정의
 def dfs(graph, v, visited):
     #현재 노드를 방문 처리
-    visited[v] = True
+    visited[v] = 1
     print(v, end=' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v] :
-        if not visited[i] :
+        if visited[i] == 0 :
             dfs(graph, i, visited)
             
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현 (2차원 리스트)    
@@ -22,7 +22,7 @@ graph = [
 ]
 
 # 각 노드가 방문된 정보를 리스트 자료형으로 표현 (1차원 리스트)
-visited = [False] * len(graph)
+visited = [0] * len(graph)
 # 정의된 DFS 함수 호출
 dfs(graph, 1, visited)
 
