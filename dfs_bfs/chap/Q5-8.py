68c8c672a3a56389e87abd57df01d62bd1e979ec
abc20210708 <gotqlc2670@naver.com>
2023-05-30 20:58:39 +0900

    Update Q5-8.py

--- dfs_bfs/chap/Q5-8.py
+++ dfs_bfs/chap/Q5-8.py
@@ -7,7 +7,7 @@ def bfs(graph, start, visited) :
     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
     # 현재 노드를 방문 처리
-    visited[start] = True
+    visited[start] = 1
     # 큐가 빌 때까지 반복
     while queue:
         # 큐에서 하나의 원소를 뽑아 출력
@@ -15,9 +15,9 @@ def bfs(graph, start, visited) :
         print(v, end=' ')
         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
         for i in graph[v]:
-            if not visited[i]:
+            if visited[i] == 0:
                 queue.append(i)
-                visited[i] = True
+                visited[i] = 1
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현 (2차원 리스트)
 graph = [
@@ -33,7 +33,7 @@ graph = [
 ]
 
 # 각 노드가 방문된 정보를 리스트 자료형으로 표현 (1차원 리스트)
-visited = [False] * 9
+visited = [0] * 9
 
 # 정의된 BFS 함수 호출
 bfs(graph, 1, visited)
