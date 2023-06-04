5166b0fb963255aff9ec22c3a6f76d4a00f04372
abc20210708 <gotqlc2670@naver.com>
2023-06-04 22:05:04 +0900

    Update Q2302.py

--- dynamic/question/silver/Q2302.py
+++ dynamic/question/silver/Q2302.py
@@ -25,3 +25,38 @@ else:
         vip = int(input())
     print(1)
 
+# 참고 블로그 https://msj725.tistory.com/129
+# https://mygumi.tistory.com/132
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+sit = [0]
+count = [0]
+# vip 석이 없으면 n 대입
+if m == 0:
+  count.append(n)
+for i in range(1,m+1):
+  sit.append(int(input()))
+  # vip 앞전으로 앉을 수 있는 자릿수
+  count.append(sit[i] - sit[i - 1] - 1)
+  # 마지막 남은 자릿수
+  if i == m:
+    count.append(n - sit[- 1])
+
+# 점화식 (123) 4 (56) 7 (89) = 3,2,2 중 최댓값인 3의 n = n-1 + n-2 를 구한다.
+maxC = max(count)
+data = [1,1]
+
+for i in range(2,maxC+1):
+  data.append(data[-2]+data[-1])
+  
+# 출력
+result = 1
+
+for i in count:
+  result = result * data[i]
+
+print(result)
