d7c4e71354fe83df566b4e94399f811c53e51a03
abc20210708 <gotqlc2670@naver.com>
2023-07-01 21:26:45 +0900

    heap 설명 추가

--- baekjoon/queue/silver/Q11279.py
+++ baekjoon/queue/silver/Q11279.py
@@ -10,6 +10,8 @@ for _ in range(int(input())):
         if len(heap) == 0:
             print(0)
         else:
+            # 힙에서 가장 작은 원소를 제거하고 그 값을 출력
             print(heapq.heappop(heap)[1])
     else:
+        # 입력받은 값 x를 음수로 변환하여 (음수, 양수)의 튜플 형태로 힙에 삽입
         heapq.heappush(heap, (-x, x)) 
