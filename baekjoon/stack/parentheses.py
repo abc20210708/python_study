7b1fa96dfba420d00ea1107072f8e9e1a9661b3d
abc20210708 <gotqlc2670@naver.com>
2023-07-03 18:35:09 +0900

    올바른 괄호 주석 추가

--- baekjoon/stack/parentheses.py
+++ baekjoon/stack/parentheses.py
@@ -2,17 +2,20 @@
 
 def solution(s):
     answer = True
-    cnt = 0
-    
-    for i in s:
-        if cnt < 0:
+    cnt = 0  # 괄호의 개수를 세는 변수
+
+    for i in s:  # 문자열 s의 각 문자를 순회
+        if cnt < 0:  # cnt가 0보다 작으면 
+            # 올바른 괄호가 아니므로 False를 반환
             return False
-        if i == '(':
-            cnt += 1
-        else:
-            cnt -=1
-    
-    if cnt < 0 or cnt > 0:
+        if i == '(':  # 현재 문자가 '('인 경우
+            cnt += 1  # cnt를 1 증가시킴 (여는 괄호)
+        else:  # 현재 문자가 ')'인 경우
+            cnt -= 1  # cnt를 1 감소시킴 (닫는 괄호)
+
+    if cnt < 0 or cnt > 0:  # 반복문 종료 후 cnt가
+        #0보다 작거나 크면 올바른 괄호가 아니므로 False를 반환
         return False
 
-    return True
+    return True  # 모든 문자 순회 후 cnt가 0인 
+                # 경우에만 올바른 괄호이므로 True를 반환
