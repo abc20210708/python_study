90ec9a982d3aa50be016d78cce1510f6b40140da
abc20210708 <gotqlc2670@naver.com>
2023-07-10 21:14:49 +0900

    큰 수 만들기 설명 추가

--- programmers/greedy/Q02.py
+++ programmers/greedy/Q02.py
@@ -5,12 +5,14 @@
 def solution(number, k):
     answer = [] # stack
 
-    for i in number:
+    for i in number:  # 숫자 문자열을 한 자리씩 반복하면서
         while k > 0 and answer and answer[-1] < i:
-            answer.pop()
-            k -= 1
-        answer.append(i)
+            # k가 0보다 크고, answer가 비어있지 않고, answer의 마지막 요소가 현재 숫자보다 작을 때
+            answer.pop()  # answer에서 마지막 요소를 제거
+            k -= 1  # 제거한 횟수를 1 감소시킴
+        answer.append(i)  # 현재 숫자를 answer에 추가
 
     return ''.join(answer[:len(answer) - k])
+    # answer에서 k만큼의 숫자를 제외한 나머지 숫자들을 문자열로 변환하여 반환
 
 print(solution("1924", 2))
