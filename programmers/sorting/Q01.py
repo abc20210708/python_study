0e6eb228c0750fd0a7775d725ef35e154a649617
abc20210708 <gotqlc2670@naver.com>
2023-07-12 16:10:53 +0900

    K번째 수 설명 추가

--- programmers/sorting/Q01.py
+++ programmers/sorting/Q01.py
@@ -4,11 +4,12 @@ def soltuion(array, commands):
     answer = []
     
     for i in commands:
-        temp = array[i[0]-1: i[1]]
-        temp.sort()
-        answer.append(temp[i[2]-1])
+        temp = array[i[0]-1: i[1]]  # array에서 i[0]부터 i[1]까지의 부분 리스트를 생성
+        temp.sort()  # 생성된 부분 리스트를 정렬
+        answer.append(temp[i[2]-1])  # 정렬된 부분 리스트에서 i[2]번째 원소를 결과 리스트에 추가
         
     return answer
+
 
 print(soltuion([1,5,2,6,3,7,4], [
     [2,5,3], [4,4,1], [1,7,3]
