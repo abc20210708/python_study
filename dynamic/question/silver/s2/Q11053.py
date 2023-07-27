e70dfe6ffbb5f84d8566cab62d336c8326895afb
abc20210708 <gotqlc2670@naver.com>
2023-07-27 19:24:16 +0900

    가장 긴 증가하는 부분 수열 (실버 2) - 참고 블로그 추가

--- dynamic/question/silver/s2/Q11053.py
+++ dynamic/question/silver/s2/Q11053.py
@@ -1,5 +1,6 @@
 ## 가장 긴 증가하는 부분 수열 (실버 2)
 #  참고 블로그 https://velog.io/@djagmlrhks3/Algorithm-BaekJoon-11053.-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-by-Python
+#  https://duckracoon.tistory.com/entry/%EB%B0%B1%EC%A4%80-11053-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-%ED%95%B4%EC%84%A4-python
 
 import sys
 input = sys.stdin.readline
