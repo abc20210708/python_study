14796e13886fbb5ff8c1e184f77007af6b7c60d1
abc20210708 <gotqlc2670@naver.com>
2023-08-07 19:07:28 +0900

    동전 2 (골드 5) - 설명 추가

--- dynamic/question/gold/Q2294.py
+++ dynamic/question/gold/Q2294.py
@@ -1,19 +1,29 @@
 ## 동전 2 (골드 5)
 #  참고 블로그 https://tesseractjh.tistory.com/137
-
-import sys
-input = sys.stdin.readline
-
+#  https://he-ya.tistory.com/24
 
 n, k = map(int, input().split())
-coin = [int(input()) for _ in range(n)]
+coins = [int(input()) for _ in range(n)]
 
-dp = [10001 for _ in range(k+1)]
+# 나올수 있는 가치보다 큰 수가 k + 1개 있는 리스트 생성
+dp = [10001] * (k+1)
+
+# 0의 가치는 동전이 필요없음
 dp[0] = 0
-for i in range(1, k+1):
-    for j in coin:
-        if i < j : continue
-        dp[i] = min(dp[i], dp[i-j]+1)
 
-if dp[k] == 10001: print(-1)
-else: print(dp[k])
+#  코인 배열의 값을 가져오고
+for num in coins:
+     # 코인의 가치부터 목표하는 금액까지를 확인하여
+    for i in range(num, k+1):
+        dp[i] = min(dp[i],dp[i-num]+1)
+    # 현재 값에서 가져온 코인 값을 빼주었을 때의 코인 사용 개수에 지금 코인 개수 하나를 더한 값과 
+    # 이전 코인들로만 조합했을 때 사용된 코인 개수를 비교하여 더 작은 값을 dp배열에 담기
+    
+    # 현재 확인하는 코인의 가치만큼 뺐을 때 다른 코인의 조합으로 만들어지지 않으면 10001이
+    # 저장되어 있을것이므로 확인하려는 자리 값이 갱신되지 않을 것
+if dp[k] == 10001:
+   print(-1)
+    # 마지막 값이 갱신된 적이 없으면 -1을 출력
+else:
+   print(dp[-1])
+    # 아니라면 마지막 값을 출력
--- dynamic/question/silver/s2/Q11053.py
+++ dynamic/question/silver/s2/Q11053.py
@@ -11,17 +11,17 @@ input = sys.stdin.readline
 # 길이를 입력
 n = int(input())
 
-arr = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
 # 각 숫자를 마지막 원소로 가지는 증가하는 부분 수열의
 # 최대 길이를 저장하는 리스트를 생성합니다.
 # 이때, 각 숫자는 0 이상이므로, table 리스트의 길이를 
 # 수열의 최대값보다 크게 설정합니다.
-table = [0] * (max(arr) + 1)
+dp = [0] * (max(nums) + 1)
 
 # 입력받은 수열을 순회하면서 각 숫자를 마지막 원소로 
 # 가지는 부분 수열의 길이를 구합니다.
-for v in arr:
+for v in nums:
     # 현재 숫자를 마지막 원소로 가지는 부분 수열 중, 
     # 그 원소보다 작은 숫자들로 구성된 부분 수열의 길이 중 최대값을 찾습니다.
     # table 리스트는 이전에 계산한 부분 수열의 길이를 저장하고 있으므로,
@@ -29,11 +29,11 @@ for v in arr:
     # 이때, 해당 인덱스의 값을 모두 비교하고 그 중 가장 큰 값에 1을 더한 값이 
     # 현재 숫자를 마지막으로 하는 부분 수열의 길이가 됩니다.
     # 따라서 table 리스트의 해당 인덱스에 현재 부분 수열의 길이를 저장합니다.
-    table[v] = max(table[:v]) + 1
+    dp[v] = max(dp[:v]) + 1
 
 # table 리스트에서 가장 큰 값이 가장 긴 증가하는 부분 수열의 길이가 됩니다.
 # 이 값을 출력합니다.
-print(max(table))
+print(max(dp))
 
 '''
 import sys
