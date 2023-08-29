46ab681a5fc01629c342e452d4a9f5b23cd52332
abc20210708 <gotqlc2670@naver.com>
2023-08-29 17:51:14 +0900

    피자 small (실버 5) - 코드 추가

--- dynamic/question/silver/s5/Q14606.py
+++ dynamic/question/silver/s5/Q14606.py
@@ -1,3 +1,10 @@
 ## 피자 small (실버 5)
+#  참고 블로그 https://jinho-study.tistory.com/971
 
-n = int(input())
+dp = [0]*(11)
+dp[1] = 0
+dp[2] = 1
+for i in range(3, N+1):
+    m = i//2
+    dp[i] = m*(i-m) + dp[m] + dp[i-m]
+print(dp[N])
