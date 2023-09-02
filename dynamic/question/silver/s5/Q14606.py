bc7544152ed4caa79a7400742b9a9c225fb4889d
abc20210708 <gotqlc2670@naver.com>
2023-09-02 15:44:02 +0900

    피자 small (실버 5) - 코드 수정

--- dynamic/question/silver/s5/Q14606.py
+++ dynamic/question/silver/s5/Q14606.py
@@ -1,10 +1,14 @@
 ## 피자 small (실버 5)
 #  참고 블로그 https://jinho-study.tistory.com/971
 
-dp = [0]*(11)
-dp[1] = 0
-dp[2] = 1
-for i in range(3, N+1):
-    m = i//2
-    dp[i] = m*(i-m) + dp[m] + dp[i-m]
-print(dp[N])
+
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 피자판의 개수
+
+if n == 1:
+    print(0)
+    exit()
+    
+print(n * (n-1) // 2)
