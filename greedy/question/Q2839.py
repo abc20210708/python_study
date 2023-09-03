ed4f2a81204de639d0a742f5251698fd43539d3c
abc20210708 <gotqlc2670@naver.com>
2023-09-03 18:08:13 +0900

    설탕 배달 (실버 4) - DP 풀이 코드 추가

--- greedy/question/Q2839.py
+++ greedy/question/Q2839.py
@@ -1,4 +1,25 @@
-# 설탕 배달
+# 설탕 배달 (실버 4)
+
+## 참고 블로그 https://jobdong7757.tistory.com/33
+
+n = int(input())
+
+d = [10001] * 5001
+d[0] = 0
+
+for i in range(3, n+1):
+    if i-5 >= 0 and d[i-5] != 10001:
+        d[i] = min(d[i], d[i-5]+1)
+    if d[i-3] != 10001:
+        d[i] = min(d[i], d[i-3]+1)
+
+if d[n] == 10001:
+    print(-1)
+else:
+    print(d[n])
+
+
+
 ## 참고 블로그 https://ooyoung.tistory.com/81
 
 sugar = int(input())
