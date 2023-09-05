00cadc266ceff940305a27986d32ba8c1d13c2ce
abc20210708 <gotqlc2670@naver.com>
2023-09-05 19:05:42 +0900

    실수형 출력 예시 추가

--- dynamic/question/silver/s4/Q2670.py
+++ dynamic/question/silver/s4/Q2670.py
@@ -6,3 +6,19 @@ arr = list(float(input()) for _ in range(N))
 for i in range(1, N):
     arr[i] = max(arr[i], arr[i] * arr[i - 1])
 print('%0.3f' % max(arr))
+
+## 실수형 출력
+#  참고 블로그 https://sylagape1231.tistory.com/42
+
+'''
+실수형 변수를 소수점 맞춰 출력하기
+
+.4f == 소수 4째 자리까지 반올림 출력
+  변수 포멧 (%d, %s, ...) 이용
+   -%를 이용한다.
+
+x = 3.141592653
+print("%.4f" % x)
+▼ 출력 결과
+3.1416
+'''
