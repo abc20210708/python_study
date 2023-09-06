33722eebf864e0c14cbe5c4b2e51a79ff7681099
abc20210708 <gotqlc2670@naver.com>
2023-09-06 18:49:31 +0900

    돌 게임 3 (실버 3) - 풀이 추가

--- dynamic/question/silver/s3/Q9657.py
+++ dynamic/question/silver/s3/Q9657.py
@@ -1,3 +1,20 @@
 ## 돌 게임 3 (실버 3)
+#  참고 블로그 https://dev-scratch.tistory.com/96
 
 n = int(input())
+
+tmp = ["SK", "CY"]
+d = [-1] * 1001
+
+d[1] = 0 # SK
+d[2] = 1 # CY
+d[3] = 0 # SK
+d[4] = 0 # SK
+
+for i in range(5, n+1):
+    if d[i-1] == d[i-3] == d[i-4] == 0:
+        d[i] = 1
+    else:
+        d[i] = 0
+
+print(tmp[d[n]])
