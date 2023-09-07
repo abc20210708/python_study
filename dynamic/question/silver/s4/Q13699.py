8e85be56e9339681dcbb653522dea5272a742c2e
abc20210708 <gotqlc2670@naver.com>
2023-09-07 16:21:55 +0900

    점화식 (실버 4) - 다른 풀이 추가

--- dynamic/question/silver/s4/Q13699.py
+++ dynamic/question/silver/s4/Q13699.py
@@ -1,5 +1,25 @@
 ## 점화식 (실버 4)
 
+## 다른 풀이
+#  참고 블로그 https://kau-algorithm.tistory.com/441
+
+n = int(input())
+d = [0] * 36
+
+d[0] = 1
+d[1] = 1
+d[2] = 2
+d[3] = 5
+
+if n > 3:
+    for i in range(4, n+1):
+        for j in range(i):
+            d[i] += d[j] * d[i-j-1]
+
+print(d[n])
+
+'''
+틀린 내가 작성한 풀이
 n = int(input())
 
 d = [1, 1, 2, 5]
@@ -10,3 +30,4 @@ else:
     for i in range(4, n+1):
         d.append((d[i-1]*2)+(d[i-2]*2))
     print(d[n])
+'''    
