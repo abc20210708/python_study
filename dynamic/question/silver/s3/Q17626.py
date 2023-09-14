f7601d34feb1eea29364f00888ef020cb5577238
abc20210708 <gotqlc2670@naver.com>
2023-09-15 08:06:00 +0900

    Four Squares (실버 3) - 다른 풀이

--- dynamic/question/silver/s3/Q17626.py
+++ dynamic/question/silver/s3/Q17626.py
@@ -20,3 +20,29 @@ for i in range(2, n+1):
     dp.append(min_value + 1)
 
 print(dp[n])
+
+
+## 다른 풀이
+#  참고 코드 https://www.acmicpc.net/source/56719874
+
+import math
+ 
+def fourSquares(n):
+    # √n이 정수일 때
+    if int(math.sqrt(n)) == math.sqrt(n):
+        return 1
+    # √(n - i^2)이 정수일 때
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if int(math.sqrt(n - i**2)) == math.sqrt(n - i**2):
+            return 2
+    # √(n - i^2 - j^2)이 정수일 때
+    for i in range(1, int(math.sqrt(n)) + 1):
+        for j in range(1, int(math.sqrt(n - i**2)) + 1):
+            if int(math.sqrt(n - i**2 - j**2)) == math.sqrt(n - i**2 - j**2):
+                return 3
+    # 남은 경우는 4
+    return 4
+ 
+ 
+n = int(input())
+print(fourSquares(n))
