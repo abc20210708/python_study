3fd4d23c6fdbcf6d884577c1adaafa80c81041dc
abc20210708 <gotqlc2670@naver.com>
2023-09-17 11:32:09 +0900

    다이나믹이 뭐예요? (실버 3) - 풀이 추가

--- dynamic/question/silver/s3/Q14494.py
+++ dynamic/question/silver/s3/Q14494.py
@@ -1,3 +1,17 @@
 ## 다이나믹이 뭐예요? (실버 3)  
 
-n, m = int(input())
+
+# 참고 블로그 https://coooco.tistory.com/60
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+#n, m 행렬 이므로 모두 다 1로 초기화
+array =[[1]* n for _ in range(m)]
+ 
+for i in range(1,m):
+    for j in range(1,n):
+        array[i][j]=(array[i-1][j-1]+array[i-1][j]+array[i][j-1])%1000000007
+ 
+print(array[m-1][n-1])
