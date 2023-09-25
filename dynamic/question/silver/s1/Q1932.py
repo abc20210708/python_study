2f83efafbd6790a5beee74405b3b18e69c07b3e0
abc20210708 <gotqlc2670@naver.com>
2023-09-25 22:41:39 +0900

    정수 삼각형 (실버 1) - 코드 추가

--- dynamic/question/silver/s1/Q1932.py
+++ dynamic/question/silver/s1/Q1932.py
@@ -1,5 +1,17 @@
 ## 정수 삼각형 (실버 1)
+#  참고 블로그 https://velog.io/@bye9/%EB%B0%B1%EC%A4%80%ED%8C%8C%EC%9D%B4%EC%8D%AC-1932-%EC%A0%95%EC%88%98-%EC%82%BC%EA%B0%81%ED%98%952
 
-n = int(input())
+n=int(input())
+d=[]
+for i in range(n):
+  d.append(list(map(int, input().split())))
 
-d = [list(map(int, input().split()))]    
+for i in range(1,n):
+  for j in range(len(d[i])):
+    if j==0:
+      d[i][j]=d[i][j]+d[i-1][j]
+    elif j==len(d[i])-1: 
+      d[i][j]=d[i][j]+d[i-1][j-1]
+    else:
+      d[i][j]=max(d[i-1][j-1],d[i-1][j])+d[i][j]
+print(max(d[n-1]))
