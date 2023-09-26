9d1f0643dee644429394e5e751fa1ea895f8536c
abc20210708 <gotqlc2670@naver.com>
2023-09-26 21:31:49 +0900

    카드1 (실버 5) - 중복 코드 수정

--- etc/q/Q2161.py
+++ etc/q/Q2161.py
@@ -11,7 +11,6 @@ for i in range(1, n+1):
 while len(d) != 1:
     print(d.pop(0), end=" ") # 2, 3, 4
     d.append(d.pop(0)) # 3, 4, 2
-    print(d.pop(0), end=" ") # 4, 2
 
 print(d[0], end=" ")
     
