98c5aa3df3ff0adb6ba33401b50b41ca9da6c7e6
abc20210708 <gotqlc2670@naver.com>
2023-09-27 18:18:00 +0900

    카드1 (실버 5) - deque 활용 풀이 추가

--- etc/q/Q2161.py
+++ etc/q/Q2161.py
@@ -1,6 +1,23 @@
 ## 카드1 (실버 5)
 
-# 1~ n
+# queue를 사용해 풀이해보기
+
+from collections import deque
+
+n = int(input())
+d = deque()
+
+for i in range(1, n+1):
+    d.append(i)
+    
+while len(d) != 1:
+    print(d.popleft(), end=" ") # 2, 3, 4
+    d.append(d.popleft())
+    
+print(d[0], end=" ")
+
+
+# stack 활용 풀이
 n = int(input())
 
 d = []
@@ -13,5 +30,3 @@ while len(d) != 1:
     d.append(d.pop(0)) # 3, 4, 2
 
 print(d[0], end=" ")
-    
-    
