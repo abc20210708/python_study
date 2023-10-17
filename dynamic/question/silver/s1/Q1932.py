2b1072e0658f6719efd0598b20db591f1f5285cc
abc20210708 <gotqlc2670@naver.com>
2023-10-17 19:59:53 +0900

    정수 삼각형 (실버 1) - 다른 풀이 추가

--- dynamic/question/silver/s1/Q1932.py
+++ dynamic/question/silver/s1/Q1932.py
@@ -1,4 +1,27 @@
 ## 정수 삼각형 (실버 1)
+
+## 다른 풀이
+#  참고 블로그 https://coooco.tistory.com/105
+import sys
+n = int(sys.stdin.readline())
+dp = []
+
+for i in range(n):
+  dp.append(list(map(int, input().split())))
+  
+for i in range(1, n):
+  for j in range(i+1):
+    if j == 0:
+      dp[i][j] += dp[i-1][j]
+    elif j == i:
+      dp[i][j] += dp[i-1][j-1]
+    else:
+      dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+  
+print(max(dp[n-1]))
+
+
+
 #  참고 블로그 https://velog.io/@bye9/%EB%B0%B1%EC%A4%80%ED%8C%8C%EC%9D%B4%EC%8D%AC-1932-%EC%A0%95%EC%88%98-%EC%82%BC%EA%B0%81%ED%98%952
 
 n=int(input())
