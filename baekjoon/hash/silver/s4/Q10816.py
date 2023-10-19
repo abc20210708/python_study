0ab9b217289482bada1ab97269f1fb875e5f4658
abc20210708 <gotqlc2670@naver.com>
2023-10-19 16:57:49 +0900

    sys.stdout.write 활용 출력

--- baekjoon/hash/silver/s4/Q10816.py
+++ baekjoon/hash/silver/s4/Q10816.py
@@ -1,4 +1,5 @@
 ## 숫자 카드 2 (실버 4)
+import sys
 
 n = int(input())
 lst = list(map(int, input().split()))
@@ -16,5 +17,10 @@ lst2 = list(map(int, input().split()))
 for i in lst2:
     if i in dic:
         print(dic.get(i), end=" ")
+        
     else:
-        print(0, end=" ")
+       print(0, end=" ")
+
+#sys.stdout.write 활용 출력
+# (str(dic.get(i)) + ' ')
+# sys.stdout.write('0' + ' ')
