847520a00bf50d82a83be9edaa3c89d321c4da34
abc20210708 <gotqlc2670@naver.com>
2023-10-19 15:36:06 +0900

    res.append((k+1)**2) 와 res.append((k+1)*(k+1))

--- implementation/question/silver/s3/Q1051.py
+++ implementation/question/silver/s3/Q1051.py
@@ -16,3 +16,7 @@ for i in range(n):
                 if (arr[i][j] == arr[i+k][j] == arr[i][j+k] == arr[i+k][j+k]):
                     res.append((k+1)**2)  
 print(max(res))
+
+## 거듭제곱 : 같은 수를 반복해서 곱한 것
+# res.append((k+1)**2) 와 res.append((k+1)*(k+1)) 
+# 둘은 같은 결과
