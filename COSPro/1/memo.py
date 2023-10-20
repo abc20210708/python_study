6ae45aa5737ca4557d7b3a16693e91ec4ac19093
abc20210708 <gotqlc2670@naver.com>
2023-10-20 20:11:43 +0900

    메모장 다른 풀이 - 반복문 활용

--- COSPro/1/memo.py
+++ COSPro/1/memo.py
@@ -1,4 +1,21 @@
 ## 메모장
+
+## 다른 풀이 - 반복문 활용
+#  참고 블로그 https://mag1c.tistory.com/319?category=1125499
+def solution(K, words):
+    if len(words) == 1: 
+        return 1
+    res = 1
+    size = 0
+    for i in range(len(words)):
+        if (size + len(words[i]) > K):
+            res += 1
+            size = 0
+        size += len(words[i]) + 1
+        
+    return res
+    
+
 #  참고 블로그 https://jaimemin.tistory.com/2109
 def solution(K, words):
     res = 0
