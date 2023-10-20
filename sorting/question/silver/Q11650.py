2154d4da82ca021407946a4d7cef951f1b30d083
abc20210708 <gotqlc2670@naver.com>
2023-10-20 21:41:14 +0900

    Update Q11650.py

--- sorting/question/silver/Q11650.py
+++ sorting/question/silver/Q11650.py
@@ -1,4 +1,6 @@
 ## 좌표 정렬하기 (실버 5)
+import sys
+input = sys.stdin.readline
 
 lst = []
 for _ in range(int(input())):
@@ -9,3 +11,10 @@ lst.sort(key=lambda x:(x[0], x[1]))
 
 for i,j in lst:
     print(i, j)
+
+'''
+import sys
+input = sys.stdin.readline
+코드 추가로
+4416ms -> 348ms 시간 단축
+'''
