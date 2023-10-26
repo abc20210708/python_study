752d2e97cb97863b193d85ec377d8dc58062a3cf
abc20210708 <gotqlc2670@naver.com>
2023-10-26 18:45:27 +0900

    두루마리 휴지 (실버 5) - 올바른 다른 풀이 추가

--- baekjoon/string/silver/s5/Q25178.py
+++ baekjoon/string/silver/s5/Q25178.py
@@ -1,33 +1,27 @@
 ## 두루마리 휴지 (실버 5)
+#  참고 블로그 https://tooy.tistory.com/71
 
 n = int(input())
 
 s1 = input().rstrip()
 s2 = input().rstrip()
 
-tmp1 = set(s1)
-tmp2 = set(s2)
+n1, n2, n3 = False, False, False
 
-if tmp1 != tmp2:
-    print("NO")
-    exit()
-if s1[0] != s2[0] or s1[-1] != s2[-1]:
-    print("NO")
-    exit()
+if sorted(s1) == sorted(s2):
+    n1 = True
+
+if s1[0] == s2[0] and s1[-1] == s2[-1]:
+    n2 = True
     
-tmp1.remove("a")
-tmp2.remove("a")
-tmp1.remove("e")
-tmp2.remove("e")
-tmp1.remove("i")
-tmp2.remove("i")
-tmp1.remove("o")
-tmp2.remove("o")
-tmp1.remove("u")
-tmp2.remove("u")
+for i in 'aeiou':
+    s1 = s1.replace(i, '')
+    s2 = s2.replace(i, '')
 
-if tmp1 != tmp2:
-    print("NO")
-    exit()
-else:
+if s1 == s2:
+    n3 = True
+
+if n1 and n2 and n3:
     print("YES")
+else:
+    print("NO")
