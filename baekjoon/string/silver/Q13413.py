5a706fcc6aa01620aee45b94b2199cb557ecc518
abc20210708 <gotqlc2670@naver.com>
2023-10-30 13:51:11 +0900

    오셀로 재배치 (실버 4)

---
+++ baekjoon/string/silver/Q13413.py
@@ -0,0 +1,23 @@
+## 오셀로 재배치 (실버 4)
+
+t = int(input())
+arr, res = [], []
+cnt = 0
+
+for i in range(t):
+    n = int(input())
+    first = input()
+    second = input()
+    
+    for i in range(n):
+        if first[i] != second[i]:
+            arr.append(first[i])
+    if arr.count('B') >= arr.count("W"):
+        cnt = arr.count("B")
+    else:
+        cnt = arr.count("W")
+    res.append(cnt)
+    arr = []
+    
+for a in res:
+    print(a)
