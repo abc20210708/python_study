eec0e753e18f1eef170e75d12a42a25191128f9f
abc20210708 <gotqlc2670@naver.com>
2023-11-03 19:00:23 +0900

    타노스 (실버 3) - 다른 풀이 추가

--- baekjoon/string/silver/s3/Q20310.py
+++ baekjoon/string/silver/s3/Q20310.py
@@ -1,8 +1,34 @@
 ## 타노스 (실버 3)
-#  참고 블로그 https://velog.io/@peacemiller/%EB%B0%B1%EC%A4%80-20310-%ED%83%80%EB%85%B8%EC%8A%A4-python
 
+# 다른 풀이
+# 참고 코드 https://www.acmicpc.net/source/68673596
 import sys
 input = sys.stdin.readline
+
+s = list(input().rstrip())
+length = len(s)
+
+one = s.count('1') // 2
+zero = s.count('0') // 4
+
+for x in range(length)[::-1]:
+    if s[x] == '0' and zero:
+        s[x] = ''
+        zero -= 1
+
+for x in range(length):
+    if s[x] == '1' and one:
+        s[x] = ''
+        one -= 1
+
+print("".join(s))
+
+
+
+
+#  참고 블로그 https://velog.io/@peacemiller/%EB%B0%B1%EC%A4%80-20310-%ED%83%80%EB%85%B8%EC%8A%A4-python
+
+
 
 s = list(input().rstrip())
 
@@ -30,5 +56,8 @@ for i in s:
         s.remove(i)
         tmp += 1
 
-for i in s[::-1]:
-    print(i, end='')
+s = s[::-1]
+print(''.join(s))
+
+#for i in s[::-1]:
+#    print(i, end='')
