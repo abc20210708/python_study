e26d587929b9f5a3fd05a9c3f2c5c50b3c9e8b91
abc20210708 <gotqlc2670@naver.com>
2023-11-10 12:15:14 +0900

    A → B (실버 2) - BFS 다른 풀이 추가

--- dfs_bfs/question/silver/Q16953.py
+++ dfs_bfs/question/silver/Q16953.py
@@ -18,3 +18,26 @@ while b != a:
         break
 else:
     print(tmp)
+    
+
+## BFS
+from collections import deque
+a, b = map(int, input().split())
+q = deque()
+q.append((a, 1))
+tmp = 0
+
+while q:
+    x, y = q.popleft()
+    
+    if x > b:
+        continue
+    if x == b:
+        print(y)
+        break
+    q.append((int(str(x)+"1"), y+1))
+    q.append((x*2, y+1))
+else:
+    print(-1)    
+
+        
