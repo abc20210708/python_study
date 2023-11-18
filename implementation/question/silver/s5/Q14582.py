2aac86a1f1f10953dbdc5fd6de303df0f7ee0509
abc20210708 <gotqlc2670@naver.com>
2023-11-18 17:10:17 +0900

    오늘도 졌다 (실버 5)

---
+++ implementation/question/silver/s5/Q14582.py
@@ -0,0 +1,22 @@
+## 오늘도 졌다 (실버 5)
+#  참고 블로그 https://woojinhong.tistory.com/m/155
+
+w = list(map(int, input().split()))
+s = list(map(int, input().split()))
+
+w_sum, s_sum = 0, 0
+cnt = 0
+
+for i in range(9):
+    w_sum += w[i]
+    
+    if w_sum > s_sum and cnt == 0:
+        cnt += 1
+    if w_sum < s_sum and cnt == 1:
+        cnt += 1
+    s_sum += s[i]
+    
+if cnt == 2 and w_sum < s_sum or cnt == 1 and w_sum < s_sum:
+    print("Yes")
+else:
+    print("No")
