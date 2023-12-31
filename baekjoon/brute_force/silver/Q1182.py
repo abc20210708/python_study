db5c984fa18e2d6cb91acdd9c12cccf3db2c1cde
abc20210708 <gotqlc2670@naver.com>
2023-12-31 17:39:45 +0900

    https://seongonion.tistory.com/98

---
+++ baekjoon/brute_force/silver/Q1182.py
@@ -0,0 +1,18 @@
+## 부분수열의 합 (실버 2)
+#  참고 블로그 https://seongonion.tistory.com/98
+
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+for i in range(1, n+1):
+    comb = combinations(arr, i)
+
+    for x in comb:
+        if sum(x) == s:
+            cnt += 1
+
+print(cnt)
