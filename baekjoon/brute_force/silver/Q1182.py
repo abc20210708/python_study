d5143b7d7fcfd4ec5b602ab3ed78c13483a6c3aa
abc20210708 <gotqlc2670@naver.com>
2024-01-01 18:15:57 +0900

    부분수열의 합 (실버 2) - 다른 풀이 추가

--- baekjoon/brute_force/silver/Q1182.py
+++ baekjoon/brute_force/silver/Q1182.py
@@ -16,3 +16,24 @@ for i in range(1, n+1):
             cnt += 1
 
 print(cnt)
+
+## 다른 풀이
+#  참고 블로그 https://velog.io/@cu1210/%EB%B0%B1%EC%A4%80-1182-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EB%B6%80%EB%B6%84%EC%88%98%EC%97%B4%EC%9D%98-%ED%95%A9
+
+N, S = map(int, input().split())
+num = list(map(int, input().split()))
+cnt = 0
+ans = []
+
+def solve(start):
+    global cnt
+    if sum(ans) == S and len(ans) > 0:
+        cnt += 1
+
+    for i in range(start, N):
+        ans.append(num[i])
+        solve(i+1)
+        ans.pop()
+
+solve(0)
+print(cnt)
