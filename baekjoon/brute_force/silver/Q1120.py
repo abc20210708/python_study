577de3c4d6571516910551a66f3b50d1f7d1f61f
abc20210708 <gotqlc2670@naver.com>
2024-01-07 00:35:49 +0900

    ## 문자열 (실버 4) - 설명 추가

--- baekjoon/brute_force/silver/Q1120.py
+++ baekjoon/brute_force/silver/Q1120.py
@@ -1,12 +1,13 @@
 ## 문자열 (실버 4)
-#  참고 블로그 https://velog.io/@yj_lee/%EB%B0%B1%EC%A4%80-1120%EB%B2%88-%EB%AC%B8%EC%9E%90%EC%97%B4-%ED%8C%8C%EC%9D%B4%EC%8D%AC
-a, b = map(str, input().split())
+#  참고 블로그 https://velog.io/@highcho/Algorithm-baekjoon-1120
+import sys
 
-res = []
-for i in range(len(b)-len(a)+1):
-    cnt = 0
-    for j in range(len(a)):
-        if a[j] != b[j+i]:
-            cnt += 1
-    res.append(cnt)
-print(min(res))
+a, b = map(str, sys.stdin.readline().strip().split())
+ret = []
+for i in range(len(b) - len(a) + 1): # a의 시작점을 변경하여 b문자열을 탐색할 수 있는 횟수
+	cnt = 0
+	for j in range(len(a)): # a 길이 만큼 전체
+		if a[j] != b[i + j]: # b 문자열의 시작점을 변경해가며 a 전체 문장 탐색
+			cnt += 1
+	ret.append(cnt)
+print(min(ret)) # 차이가 가장 적은 경우 선택
