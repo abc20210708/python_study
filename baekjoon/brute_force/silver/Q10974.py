d2a99ce753aeca6258b8085801e1a8313ef7fc2e
abc20210708 <gotqlc2670@naver.com>
2024-01-08 16:10:40 +0900

    모든 순열 (실버 3) - 다른 풀이 추가

--- baekjoon/brute_force/silver/Q10974.py
+++ baekjoon/brute_force/silver/Q10974.py
@@ -10,3 +10,20 @@ tmp = list(permutations(lst, cnt))
 
 for i in tmp:
     print(*i)
+    
+## 다른 풀이
+#  참고 블로그 https://honggom.tistory.com/115
+n = int(input())
+temp = []
+
+def dfs():
+    if len(temp) == n:
+        print(*temp)
+        return
+    for i in range(1, n + 1):
+        if i not in temp:
+            temp.append(i)
+            dfs()
+            temp.pop()
+
+dfs()
