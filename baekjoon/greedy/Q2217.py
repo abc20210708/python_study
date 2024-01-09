03a284be6b07fb97be1c7479664a55d9c33dc2cc
abc20210708 <gotqlc2670@naver.com>
2024-01-09 17:06:32 +0900

    로프 (실버 4) - 다른 풀이 추가

--- baekjoon/greedy/Q2217.py
+++ baekjoon/greedy/Q2217.py
@@ -1,3 +1,5 @@
+## 로프 (실버 4)
+
 n = int(input())
 w = []
 res = 0
@@ -13,3 +15,23 @@ for i in range(len(w)):
     n -= 1
     
 print(res)
+
+## 다른 풀이
+#  참고 코드 https://www.acmicpc.net/source/55728690
+
+#2217 로프
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+lst=[0 for i in range(10001)]
+ans=[]
+for i in range(n):
+    lst[int(input())]+=1
+
+for i in range(len(lst)):
+    if lst[i]!=0:
+        ans.append(i*n)
+        n-=lst[i]
+
+print(max(ans))
