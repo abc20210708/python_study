6fe980110594020b155285298125d67cc255b921
abc20210708 <gotqlc2670@naver.com>
2024-03-30 08:33:09 +0900

    연구소 - 다른 풀이 추가(BFS 사용)

--- dfs_bfs/ex/Q16.py
+++ dfs_bfs/ex/Q16.py
@@ -1,6 +1,85 @@
 ## 연구소
 
-# 세로 n, 가로 m
+## 참고 코드 https://www.acmicpc.net/source/67948636
+# itertools 를 사용x, 조합으로
+# 벽을 설치하고, 퍼져야함
+# 일단 맵을 입력 받고, 벽을 설치할 수 있는 경우들에 대해서
+# deepcopy한 후 설치
+
+from copy import deepcopy
+from collections import deque
+
+n, m = map(int, input().split())
+
+# 그래프를 입력 받으면서 조합할 빈칸들의 리스트를 만들어야한다.
+virus_map = []
+blank_point = []
+virus_point = []
+
+# bfs를 할 때 사용할 방향을 지정
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for i in range(n):
+    line = list(map(int, input().split()))
+    virus_map.append(line)
+    for j in range(m):
+        if line[j] == 0:
+            blank_point.append((i, j))
+        if line[j] == 2:
+            virus_point.append((i, j))
+            
+def comb(arr, n):
+    res = []
+    if n == 0:
+        return [[]]
+    for i in range(len(arr)):
+        tmp = arr[i]
+        for j in comb(arr[i+1:], n-1):
+            res.append([tmp] + j)
+    return res
+
+# 조합 함수를 선언하고 설치 가능한 벽의 경우들을 후보 리스트로 만든다
+cands =comb(blank_point, 3)
+
+# bfs를 통해 바이러스를 확장시킨다. 이때, 각 후보들의 경우에 대해서
+# virus_map이 바뀌지 않도록 deepcopy 이용
+
+def bfs(graph):
+    q = deque(virus_point)
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                q.append((nx, ny))
+    # bfs를 통해 바이러스로 감연시킨 후 안전구역의 값을 구한다.
+    safe_zone = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                safe_zone += 1
+    # 안전 구역의 값을 반환하고
+    return safe_zone
+
+answer = 0
+for cand in cands:
+    copy_map = deepcopy(virus_map)
+    # 벽을 세우는 과정
+    for point in cand:
+        a, b = point
+        copy_map[a][b] = 1
+    # 벽을 세운 후 그래프에 bfs를 통해 safe_zone을 구한다
+    answer = max(answer, bfs(copy_map))
+    
+print(answer)
+
+
+
+'''
+# 책
 n, m = map(int, input().split())
 data = [] # 초기 맵 리스트
 tmp = [[0] * m for _ in range(n)] 
@@ -66,3 +145,4 @@ def dfs(cnt):
                 
 dfs(0)
 print(res)
+'''
