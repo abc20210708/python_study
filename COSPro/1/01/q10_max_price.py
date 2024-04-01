ebe6a81b193f5cd3418892bf4c39bedb56541e82
abc20210708 <gotqlc2670@naver.com>
2024-04-01 21:16:40 +0900

    문제10) 설명 추가

--- COSPro/1/01/q10_max_price.py
+++ COSPro/1/01/q10_max_price.py
@@ -1,14 +1,21 @@
 ## 문제 10) 주식으로 최대 수익을 내세요.
 
 def solution(prices):
+    # 무한대 값 설정
     INF = 1000000001
+    # 이전 가격을 저장할 변수 초기화
     tmp = INF
+    # 최대 이익을 저장할 변수 초기화
     answer = -INF
-    
+    # 모든 주식 가격에 대해 반복
     for price in prices:
+        # 이전 가격이 무한대가 아니면
         if tmp != INF:
-            answer = max(answer, price-tmp)
+            # 현재 주식 가격에서 이전 가격을 뺀 값과 기존 최대 이익 중 더 큰 값을 선택하여 최대 이익 갱신
+            answer = max(answer, price - tmp)
+        # 이전 가격 갱신
         tmp = min(tmp, price)
+    # 최대 이익 반환
     return answer
 
 prices1 = [1, 2, 3];
