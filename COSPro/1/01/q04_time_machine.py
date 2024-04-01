0729589662c7ca7d1efd6269eec8441f5d24752f
abc20210708 <gotqlc2670@naver.com>
2024-04-01 20:42:50 +0900

    문제4) 타임머신

---
+++ COSPro/1/01/q04_time_machine.py
@@ -0,0 +1,5 @@
+## 문제4) 타임머신
+#  참고 블로그 https://velog.io/@corone_hi/COS-PRO-1%EA%B8%89-1%EC%B0%A8-%EB%AC%B8%EC%A0%9C4-%ED%83%80%EC%9E%84%EB%A8%B8%EC%8B%A0
+
+def solution(num):
+    return str(num+1).replace('0', '1')
