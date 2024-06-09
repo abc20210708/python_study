c81442d4cf655b6c538ff04669b1af4aa9413902
abc20210708 <gotqlc2670@naver.com>
2024-06-09 19:34:28 +0900

    9372 - 상근이의 여행 (실버 4)

---
+++ baekjoon/sw/Q9372.py
@@ -0,0 +1,12 @@
+## 상근이의 여행 (실버 4)
+#  참고 블로그 https://harang1412.tistory.com/744
+
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    for i in range(m):
+        a, b = map(int, input().split())
+    print(n-1)
