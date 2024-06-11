4e8f590791a4da01aae6580dcb23f66aceb434cf
abc20210708 <gotqlc2670@naver.com>
2024-06-11 09:50:44 +0900

    9372 - 상근이의 여행 (실버 4) - 다른 풀이 추가

--- sw/s/4/Q9372.py
+++ sw/s/4/Q9372.py
@@ -1,9 +1,36 @@
 ## 상근이의 여행 (실버 4)
+#  참고 블로그 https://jinho-study.tistory.com/889
+import sys
+input = sys.stdin.readline
+
+def dfs(idx, cnt):
+    
+    chk[idx] = 1
+    
+    for n in graph[idx]:
+        if chk[n] == 0:
+            cnt = dfs(n, cnt+1)
+    return cnt
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n+1)]
+    
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    chk = [0] * (n+1)
+    cnt = dfs(1, 0)
+    print(cnt)
+
+
+
 #  참고 블로그 https://harang1412.tistory.com/744
 
 
-import sys
-input = sys.stdin.readline
+
 
 for _ in range(int(input())):
     n, m = map(int, input().split())
